1bac0394e9ed1f2b62b008c52cb9077830d3474e
林集 <16908091@qq.com>
2017-04-12 20:58:36 +0800

    fix toast bug

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -10,61 +10,61 @@ class Button extends Component {
     const disabled = ('disabled' in props || isDisabled);
 
     const classes = classnames({
-      'ui-button'        : true,
-      'block'            : ('block' in props || isBlock),
-      'radius'           : ('radius' in props || isRadius),
-      'round'            : ('round' in props || isRound),
-      'circle'           : ('circle' in props || isCircle),
-      'bordered'         : ('bordered' in props || isBordered),
-      'active'           : ('active' in props || isActive),
-      'focus'            : ('focus' in props || isFocus),
-      'disabled'         : disabled,
-      [`theme-${theme}`] : !!theme,
-      [`size-${size}`]   : !!size,
-      [className]        : !!className,
+      'ui-button': true,
+      block: ('block' in props || isBlock),
+      radius: ('radius' in props || isRadius),
+      round: ('round' in props || isRound),
+      circle: ('circle' in props || isCircle),
+      bordered: ('bordered' in props || isBordered),
+      active: ('active' in props || isActive),
+      focus: ('focus' in props || isFocus),
+      disabled,
+      [`theme-${theme}`]: !!theme,
+      [`size-${size}`]: !!size,
+      [className]: !!className,
     });
 
-    let textContent = ('loading' in props || isLoading)
-                    ? <span><Icon type="loading" className="rotate360" /> {children}</span>
-                    : children;
+    const textContent = ('loading' in props || isLoading)
+                      ? <span><Icon type="loading" className="rotate360" /> {children}</span>
+                      : children;
     return (
-      <button {...others} type={type} className={classes} disabled={disabled} onClick={(e) => !disabled && onClick(e)}>{textContent}</button>
+      <button {...others} type={type} className={classes} disabled={disabled} onClick={e => !disabled && onClick(e)}>{textContent}</button>
     );
   }
 }
 
 Button.propTypes = {
-  type      : PropTypes.string,
-  theme     : PropTypes.oneOf(['default', 'info', 'success', 'warning', 'error']),
-  size      : PropTypes.oneOf(['xl', 'lg', 'sm', 'xs']),
-  isBlock   : PropTypes.bool,
-  isRadius  : PropTypes.bool,
-  isRound   : PropTypes.bool,
-  isCircle  : PropTypes.bool,
+  type: PropTypes.string,
+  theme: PropTypes.oneOf(['default', 'info', 'success', 'warning', 'error']),
+  size: PropTypes.oneOf(['xl', 'lg', 'sm', 'xs']),
+  isBlock: PropTypes.bool,
+  isRadius: PropTypes.bool,
+  isRound: PropTypes.bool,
+  isCircle: PropTypes.bool,
   isBordered: PropTypes.bool,
-  isActive  : PropTypes.bool,
-  isFocus   : PropTypes.bool,
+  isActive: PropTypes.bool,
+  isFocus: PropTypes.bool,
   isDisabled: PropTypes.bool,
-  isLoading : PropTypes.bool,
-  className : PropTypes.string,
-  onClick   : PropTypes.func,
+  isLoading: PropTypes.bool,
+  className: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 Button.defaultProps = {
-  type      : 'button',
-  theme     : 'default',
-  size      : null,
-  isBlock   : false,
-  isRadius  : false,
-  isRound   : false,
-  isCircle  : false,
+  type: 'button',
+  theme: 'default',
+  size: null,
+  isBlock: false,
+  isRadius: false,
+  isRound: false,
+  isCircle: false,
   isBordered: false,
-  isActive  : false,
-  isFocus   : false,
+  isActive: false,
+  isFocus: false,
   isDisabled: false,
-  isLoading : false,
-  className : null,
-  onClick   : () => {},
+  isLoading: false,
+  className: null,
+  onClick: () => {},
 };
 
 export default Button;
