7f327556dc3ca3bf4c7abc6f9b77d5631478ac67
林集 <16908091@qq.com>
2017-04-25 11:48:21 +0800

    button样式更新

--- examples/decorators/withContext.js
+++ examples/decorators/withContext.js
@@ -1,5 +1,6 @@
 
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 function withContext(ComposedComponent) {
   return class WithContext extends Component {
