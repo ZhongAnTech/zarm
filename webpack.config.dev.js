ac582a5e809dffceb7d71cf6ade72d92e6bcf3d8
Jerome Lin <jerome.lin@zhongan.com>
2017-05-12 14:20:01 +0800

    #

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,37 +1,39 @@
-var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var config = require('./webpack.config.base');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const config = require('./webpack.config.base');
 
 config.devtool = 'cheap-module-eval-source-map';
+
 config.entry = {
   index: [
     'webpack-dev-server/client?http://127.0.0.1:3000',
     'webpack/hot/only-dev-server',
-    './examples/index.js'
-  ]
+    './examples/index.js',
+  ],
 };
+
 config.plugins.push(new ExtractTextPlugin('stylesheet/[name].css', {
-  allChunks: true
+  allChunks: true,
 }));
 config.plugins.push(new webpack.optimize.CommonsChunkPlugin({
-  name: 'vendors'
+  name: 'vendors',
 }));
-config.module.loaders.push({
-  test: /\.(js|jsx)$/, 
+config.module.rules.loaders.push({
+  test: /\.(js|jsx)$/,
   loader: 'react-hot!babel',
-  exclude: /node_modules/
+  exclude: /node_modules/,
 });
 config.plugins.push(new webpack.HotModuleReplacementPlugin());
 config.plugins.push(new webpack.DefinePlugin({
-  "process.env": {
-    NODE_ENV: JSON.stringify("development")
+  'process.env': {
+    NODE_ENV: JSON.stringify('development'),
   },
-  __DEBUG__: true
+  __DEBUG__: true,
 }));
 
-for (var key in config.entry) {
-  if (key == 'vendors') {
+for (let key in config.entry) {
+  if (key === 'vendors') {
     continue;
   }
   config.plugins.push(new HtmlWebpackPlugin({
