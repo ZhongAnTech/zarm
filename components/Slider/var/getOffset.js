a601df4b5637bcf94369498096f193a111e5418b
lsert <zqc200789789@126.com>
2017-05-16 14:23:23 +0800

    获取位移

---
+++ components/Slider/var/getOffset.js
@@ -0,0 +1,21 @@
+export default function getOffset(dom) {
+    if (!dom) {
+        return null;
+    }
+    let offsetLeft = [];
+    function loop(elem) {
+        offsetLeft.push({
+            left: elem.offsetLeft,
+            top: elem.offsetTop
+        });
+        if (elem.offsetParent) {
+            loop(elem.offsetParent);
+        }
+    }
+    loop(dom);
+    return offsetLeft.reduce((prev, value) => {
+        prev.left += value.left;
+        prev.top += value.top;
+        return prev;
+    }, { left: 0, top: 0 })
+}
