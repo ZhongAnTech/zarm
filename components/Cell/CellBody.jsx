b11155604da8980e5af49ad8d84aba2b62647c29
xiaoshuang.li <xiaoshuang.li@cloudinward.com>
2017-06-04 22:08:54 +0800

    add CellValidate

---
+++ components/Cell/CellBody.jsx
@@ -0,0 +1,110 @@
+
+import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
+
+import Cell from '../Cell';
+
+class CellBody extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: '',
+      type: '',
+      validate: false,
+      dirty: false,
+    };
+    this.setResult = this.setResult.bind(this);
+  }
+
+  setResult(value, validate, opts) {
+    const { getResult } = this.props;
+    const { type } = opts;
+
+    this.setState({
+      value,
+      validate,
+      type,
+      dirty: type !== 'init',
+    }, () => {
+      getResult && getResult(value, validate, opts);
+    });
+  }
+
+  helpRender() {
+    const { validate, dirty, type } = this.state;
+    const { showHelp, help = null } = this.props;
+
+    if (showHelp === 'always') {
+      return help;
+    }
+
+    if (!validate && dirty) {
+      if (showHelp === 'change') {
+        return help;
+      } else if (showHelp === type) {
+        return help;
+      }
+    }
+
+    return null;
+  }
+
+  render() {
+    const { children, className, showHelp, getResult, ...otherOpts } = this.props;
+    // cell
+    const { type, theme, icon, title, description, help, ...others } = otherOpts;
+    // validInput
+    const { max, min, maxLength, minLength, required, func, pattern, onChange, onBlur, value, ...rest } = others;
+
+    const cls = classnames({
+      'ui-cell-form': true,
+      [className]: !!className,
+    });
+
+    const CellConfig = {
+      type,
+      theme,
+      icon,
+      title,
+      description,
+      ...rest,
+    };
+
+    const ValidInputConfig = {
+      max,
+      min,
+      maxLength,
+      minLength,
+      required,
+      func,
+      pattern,
+      onChange,
+      onBlur,
+      value,
+      ...rest,
+    };
+
+    return (
+      <Cell className={cls} help={this.helpRender()} {...CellConfig}>
+        <Cell.Input setResult={this.setResult} {...ValidInputConfig}>
+          {children}
+        </Cell.Input>
+      </Cell>
+    );
+  }
+}
+
+CellBody.propTypes = {
+  type: PropTypes.oneOf(['normal', 'link', 'select']),
+  className: PropTypes.string,
+  showHelp: PropTypes.oneOf(['always', 'change', 'blur']), // 显示help的时间点，always(一直显示)，change(修改就显示)，blur(失去焦点显示)
+};
+
+CellBody.defaultProps = {
+  type: 'normal',
+  className: null,
+  showHelp: 'change',
+};
+
+export default CellBody;
---
+++ components/Cell/CellInput.jsx
@@ -0,0 +1,157 @@
+import React, { Component, PropTypes, cloneElement } from 'react';
+
+import * as validateFuncs from './validate';
+
+const typeValidateAttrs = {
+  number: ['min', 'max', 'required', 'func'],
+  bool: ['required', 'func'],
+  string: ['minLength', 'maxLength', 'pattern', 'required', 'func'],
+  all: ['min', 'max', 'minLength', 'maxLength', 'pattern', 'required', 'func'],
+};
+
+class CellInput extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { value: props.value || this._getDefaultValue() };
+    this.onChange = this.onChange.bind(this);
+    this.onBlur = this.onBlur.bind(this);
+  }
+
+  componentDidMount() {
+    this._initResult();
+  }
+
+  onChange(e) {
+    this._setResult({
+      e,
+      type: 'change',
+      cb: this.props.onChange,
+    });
+  }
+
+  onBlur(e) {
+    this._setResult({
+      e,
+      type: 'blur',
+      cb: this.props.onBlur,
+    });
+  }
+
+  _initResult() {
+    this._setResult({
+      type: 'init',
+    });
+  }
+
+  _setResult({ e, type, cb } = {}) {
+    const { name, setResult } = this.props;
+    const value = e ? this._getValue(e) : this.state.value;
+    const res = this._validate(value);
+    const opts = {
+      type,
+      name,
+    };
+
+    cb && cb(value, res);
+    setResult && setResult(value, res, opts);
+  }
+
+  _getDefaultValue() {
+    const { type } = this.props;
+
+    if (type === 'number') {
+      return 0;
+    }
+
+    if (type === 'bool') {
+      return false;
+    }
+
+    if (type === 'string') {
+      return '';
+    }
+
+    if (type === 'all') {
+      return '';
+    }
+
+    return '';
+  }
+
+  _validate(value) {
+    const { type = 'all' } = this.props;
+    const attrs = typeValidateAttrs[type] || [];
+
+    if (!required && (value === '' || value === undefined)) {
+      return true;
+    }
+
+    for (let v = 0; v < attrs.length; v++) {
+      const attr = attrs[v];
+      const args = this.props[attr];
+      const validateFunc = validateFuncs[attr](args);
+
+      if (!validateFunc(value)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  _getValue(e) {
+    e = e.target || e;
+    const value = e.value === undefined ? e : e.value;
+
+    this.setState({ value });
+    return value;
+  }
+
+  getChildren() {
+    const { onChange, onBlur, props, state } = this;
+    const { children } = props;
+    const { value } = state;
+
+    return React.Children.map(children, (child) => {
+      return cloneElement(child, {
+        onChange,
+        onBlur,
+        value,
+      }, child.props.children);
+    });
+  }
+
+  render() {
+    return (
+      <span>
+        {this.getChildren()}
+      </span>
+    );
+  }
+}
+
+CellInput.propTypes = {
+  max: PropTypes.number,
+  min: PropTypes.number,
+  maxLength: PropTypes.number,
+  minLength: PropTypes.number,
+  required: PropTypes.bool,
+  func: PropTypes.func,
+  // pattern: 正则表达式
+
+  onChange: PropTypes.func,
+  onBlur: PropTypes.func,
+
+  setResult: PropTypes.func, // 获取value和验证结果
+  resultId: PropTypes.string, // 结果储存在内存的Id
+};
+
+CellInput.defaultProps = {
+  onChange: () => {},
+  onBlur: () => {},
+  resultId: '',
+};
+
+export default CellInput;
