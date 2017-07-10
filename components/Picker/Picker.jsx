0400dd2cc955af3ead801c54707adc940ff94730
林集 <16908091@qq.com>
2017-07-10 16:29:38 +0800

    proptypes fix bug

--- components/Picker/Picker.jsx
+++ components/Picker/Picker.jsx
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
 import { arrayTreeFilter, formatToInit } from './utils';
 import ColumnGroup from './ColumnGroup';
