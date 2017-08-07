88cf17ba1947d8dd7b8811e093054b2bd06ecb51
林集 <16908091@qq.com>
2017-08-07 10:21:09 +0800

    remove Pannel theme

--- components/Panel/Panel.jsx
+++ components/Panel/Panel.jsx
@@ -4,12 +4,11 @@ import classnames from 'classnames';
 
 class Panel extends PureComponent {
   render() {
-    const { prefixCls, className, theme, children, ...others } = this.props;
+    const { prefixCls, className, children, ...others } = this.props;
 
     const cls = classnames({
       [`${prefixCls}`]: true,
       [className]: !!className,
-      [`theme-${theme}`]: !!theme,
     });
 
     return <div {...others} className={cls}>{children}</div>;
@@ -19,13 +18,11 @@ class Panel extends PureComponent {
 Panel.propTypes = {
   prefixCls: PropTypes.string,
   className: PropTypes.string,
-  theme: PropTypes.oneOf(['default', 'primary', 'info', 'success', 'warning', 'error']),
 };
 
 Panel.defaultProps = {
   prefixCls: 'za-panel',
   className: null,
-  theme: null,
 };
 
 export default Panel;
