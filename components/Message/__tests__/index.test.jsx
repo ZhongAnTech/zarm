84eb3469492a4fd9b17faaae57011d4966197513
林集 <16908091@qq.com>
2017-08-28 16:57:51 +0800

    Message tests

--- components/Message/__tests__/index.test.jsx
+++ components/Message/__tests__/index.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Message from '../index';
 
@@ -7,5 +7,30 @@ describe('Message', () => {
   it('renders correctly', () => {
     const wrapper = render(<Message>foo</Message>);
     expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('theme', () => {
+    const wrapper = render(<Message theme="error">foo</Message>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('icon', () => {
+    const wrapper = render(<Message icon={<img alt="" src="https://zhongantecheng.github.io/zarm/images/state.18e78939.png" />}>foo</Message>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('hasClosable', () => {
+    const wrapper = shallow(<Message hasClosable>foo</Message>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.find('Icon').simulate('click');
+    expect(wrapper.state('visible')).toEqual(false);
+  });
+
+  it('hasArrow', () => {
+    const onClick = jest.fn();
+    const wrapper = shallow(<Message hasArrow onClick={onClick}>foo</Message>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.simulate('click');
+    expect(onClick).toBeCalled();
   });
 });
