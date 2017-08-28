dc9cbc5b8493a15c6ea575ad33a04f4d8b906fcd
Jerome Lin <jerome.lin@zhongan.com>
2017-08-28 17:42:36 +0800

    Button tests

--- components/Button/__tests__/index.test.jsx
+++ components/Button/__tests__/index.test.jsx
@@ -8,4 +8,39 @@ describe('Button', () => {
     const wrapper = render(<Button>foo</Button>);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
+
+  it('theme', () => {
+    const wrapper = render(<Button theme="primary">foo</Button>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('block', () => {
+    const wrapper = render(<Button block>foo</Button>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('bordered', () => {
+    const wrapper = render(<Button bordered>foo</Button>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('size', () => {
+    const wrapper = render(<Button size="lg">foo</Button>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('shape is radius', () => {
+    const wrapper = render(<Button shape="radius">foo</Button>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('icon', () => {
+    const wrapper = render(<Button icon={<img alt="" src="https://zhongantecheng.github.io/zarm/images/state.18e78939.png" />}>foo</Button>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('loading', () => {
+    const wrapper = render(<Button loading>foo</Button>);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
 });
