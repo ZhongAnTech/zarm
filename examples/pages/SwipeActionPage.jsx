daee6e4b007cb745efe7a57fae687703ac0d31b7
林集 <16908091@qq.com>
2017-09-04 10:56:48 +0800

    add drag

--- examples/pages/SwipeActionPage.jsx
+++ examples/pages/SwipeActionPage.jsx
@@ -2,26 +2,10 @@ import React, { Component } from 'react';
 import Container from '../components/Container';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { Panel, Cell, SwipeAction, Drag } from '../../components';
+import { Panel, Cell, SwipeAction, SwipeActions } from '../../components';
 
 class Page extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      offsetLeft: 0,
-    };
-  }
-
   render() {
-    const { offsetLeft } = this.state;
-    const style = {
-      WebkitTransitionDuration: 0,
-      transitionDuration: 0,
-      WebkitTransform: `translate3d(${offsetLeft}px, 0, 0)`,
-      transform: `translate3d(${offsetLeft}px, 0, 0)`,
-    };
-
     return (
       <Container className="swipeAction-page">
         <Header title="滑动操作 SwipeAction" />
@@ -63,8 +47,7 @@ class Page extends Component {
                 <Cell>右滑看看</Cell>
               </SwipeAction>
 
-              <SwipeAction
-                autoClose
+              <SwipeActions
                 left={[
                   {
                     theme: 'info',
@@ -88,28 +71,15 @@ class Page extends Component {
                     text: '右按钮2',
                     onClick: () => console.log('右按钮2'),
                   },
-                ]}>
-                <Cell>左右都能滑动（自动关闭）</Cell>
-              </SwipeAction>
-
-              <Drag
-                onDragStart={() => {
-
+                ]}
+                onOpen={() => {
+                  console.log('open')
                 }}
-                onDragMove={({ translateX, offsetX, distanceX }) => {
-                  // console.log(offsetX, distanceX, translateX)
-                  if (offsetX > 0 && distanceX > 100) return false;
-                  if (translateX > 100) return false;
-
-                  this.setState({ offsetLeft: translateX + offsetX });
-                  return true;
-                }}
-                onDragEnd={() => {
-
+                onClose={() => {
+                  console.log('close')
                 }}>
-                <Cell style={style}>Drag</Cell>
-              </Drag>
-
+                <Cell>左右都能滑动（自动关闭）</Cell>
+              </SwipeActions>
             </Panel.Body>
           </Panel>
         </main>
