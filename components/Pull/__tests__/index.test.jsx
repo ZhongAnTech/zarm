7aa62c6a5173266666ea6c9d95769f6b1434534c
Jerome Lin <jerome.lin@zhongan.com>
2017-09-11 15:40:47 +0800

    Pull API

--- components/Pull/__tests__/index.test.jsx
+++ components/Pull/__tests__/index.test.jsx
@@ -15,7 +15,7 @@ describe('Pull', () => {
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
-  it('loading', () => {
+  it('refreshing', () => {
     const wrapper = shallow(
       <Pull>
         <div>1</div>
@@ -23,6 +23,47 @@ describe('Pull', () => {
         <div>3</div>
       </Pull>
     );
-    wrapper.setProps({ loading: true });
+    wrapper.setProps({ refreshing: true });
+  });
+
+  it('custom', () => {
+    const onRefresh = jest.fn();
+    const wrapper = shallow(
+      <Pull
+        moveDistance={80}
+        onRefresh={onRefresh}
+        pullDownRender={(actionState, percent) => {
+          const cls = 'custom-control';
+          switch (actionState) {
+            case 'pull':
+              return (
+                <div className={cls}>下拉刷新 {percent}</div>
+              );
+
+            case 'drop':
+              return (
+                <div className={cls}>释放刷新</div>
+              );
+
+            case 'loading':
+              return (
+                <div className={cls}>加载中</div>
+              );
+
+            case 'success':
+              return (
+                <div className={cls}>加载成功</div>
+              );
+
+            default:
+              return null;
+          }
+        }}>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </Pull>
+    );
+    wrapper.setProps({ refreshing: true });
   });
 });
