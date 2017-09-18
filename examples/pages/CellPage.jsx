3517136e593d07c66aed12e60fa2dc6eb01a297b
林集 <16908091@qq.com>
2017-09-18 18:30:26 +0800

    add Slider

--- examples/pages/CellPage.jsx
+++ examples/pages/CellPage.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Container from '../components/Container';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { Panel, Icon, Cell, Message, Input } from '../../components';
+import { Panel, Icon, Cell, Message, Input, Slider } from '../../components';
 import '../styles/pages/CellPage';
 
 const img = require('../images/icons/state.png');
@@ -18,6 +18,7 @@ class Page extends Component {
             <Panel.Header title="普通" />
             <Panel.Body>
               <Cell title="标题文字" />
+              <Cell title="标题文字"><Slider /></Cell>
             </Panel.Body>
           </Panel>
 
