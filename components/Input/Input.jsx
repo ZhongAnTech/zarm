3eb72569042b9f61400fffde1320250f5c6d7db2
林集 <16908091@qq.com>
2017-10-25 14:25:28 +0800

    input add number

--- components/Input/Input.jsx
+++ components/Input/Input.jsx
@@ -1,133 +1,33 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
-import Autosize from 'autosize';
+import InputNumber from './InputNumber';
+import InputText from './InputText';
+import InputTextarea from './InputTextarea';
 
 class Input extends PureComponent {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      length: (props.value || props.defaultValue || '').length,
-    };
-  }
-
-  componentDidMount() {
-    this.initAutosize();
-  }
-
-  componentDidUpdate(prevProps) {
-    this.updateAutosize(prevProps);
-  }
-
-  componentWillUnmount() {
-    this.destroyAutosize();
-  }
-
-  onInputChange = (e) => {
-    const { onChange } = this.props;
-    this.setState({
-      length: e.target.value.length,
-    });
-    typeof onChange === 'function' && onChange(e);
-  }
-
-  // 初始化自适应高度
-  initAutosize = () => {
-    const { type, autosize } = this.props;
-    (type === 'textarea') && autosize && Autosize(this.input);
-  }
-
-  updateAutosize = (prevProps) => {
-    if (prevProps.type !== 'textarea') return;
-    if (prevProps.style !== this.props.style || prevProps.className !== this.props.className) {
-      Autosize.update(this.input);
-    }
-  }
-
-  // 销毁自适应高度
-  destroyAutosize = () => {
-    const { type, autosize } = this.props;
-    (type === 'textarea') && autosize && Autosize.destroy(this.input);
-  }
-
   render() {
-    const {
-      prefixCls,
-      className,
-      placeholder,
-      type,
-      maxLength,
-      disabled,
-      autosize,
-      showLength,
-      ...others
-    } = this.props;
+    const { type, ...others } = this.props;
 
-    const cls = classnames({
-      [`${prefixCls}`]: true,
-      [className]: !!className,
-      disabled,
-    });
+    switch (type) {
+      case 'number':
+        return <InputNumber {...others} />;
 
-    const inputRender = (type === 'textarea')
-      ? (
-        <textarea
-          {...others}
-          ref={(ele) => { this.input = ele; }}
-          className={cls}
-          placeholder={placeholder}
-          disabled={disabled}
-          maxLength={maxLength}
-          onChange={this.onInputChange}
-          />
-      )
-      : (
-        <input
-          {...others}
-          ref={(ele) => { this.input = ele; }}
-          type={type}
-          className={cls}
-          placeholder={placeholder}
-          disabled={disabled}
-          maxLength={maxLength}
-          onChange={this.onInputChange}
-          />
-      );
+      case 'textarea':
+        return <InputTextarea {...others} />;
 
-    const valueRender = (type === 'date') && <div className={`${prefixCls}-placeholder`}>{placeholder}</div>;
-    const textLengthRender = (showLength && maxLength) && <div className={`${prefixCls}-length`}>{`${this.state.length}/${maxLength}`}</div>;
-
-    return (
-      <div className={cls}>
-        {valueRender}
-        {inputRender}
-        {textLengthRender}
-      </div>
-    );
+      default:
+        return <InputText {...others} />;
+    }
   }
 }
 
 Input.propTypes = {
-  prefixCls: PropTypes.string,
-  className: PropTypes.string,
   type: PropTypes.string,
-  value: PropTypes.string,
-  defaultValue: PropTypes.string,
-  disabled: PropTypes.bool,
-  rows: PropTypes.number,
-  autosize: PropTypes.bool,
-  maxLength: PropTypes.number,
-  showLength: PropTypes.bool,
-  onChange: PropTypes.func,
 };
 
 Input.defaultProps = {
-  prefixCls: 'za-input',
   type: 'text',
-  disabled: false,
-  autosize: false,
-  showLength: false,
 };
 
 export default Input;
