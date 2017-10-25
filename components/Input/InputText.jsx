43ba4400c9699e85462d1c7a6065b60748ace273
Jerome Lin <jerome.lin@zhongan.com>
2017-10-25 17:52:24 +0800

    input-number

--- components/Input/InputText.jsx
+++ components/Input/InputText.jsx
@@ -5,9 +5,8 @@ import classnames from 'classnames';
 class InputText extends PureComponent {
 
   render() {
-    const { prefixCls, className, disabled, ...others } = this.props;
+    const { prefixCls, className, disabled, focused, ...others } = this.props;
     const cls = classnames(prefixCls, `${prefixCls}-text`, className, disabled);
-
     return (
       <div className={cls}>
         <input
