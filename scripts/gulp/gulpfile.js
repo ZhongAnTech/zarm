c7e543a32bc126fe7766ea301017fceaa2b4ec56
Jerome Lin <jerome.lin@zhongan.com>
2017-11-09 18:04:17 +0800

    gulp task

--- scripts/gulp/gulpfile.js
+++ scripts/gulp/gulpfile.js
@@ -29,32 +29,34 @@ const CONFIG = {
   },
 };
 
-gulp.task('default', () => {
+gulp.task('copySass', () => {
   return gulp.src(CONFIG.src)
     .pipe(gulp.dest(CONFIG.lib));
-
-    // .pipe(sourcemaps.init())
-    // .pipe(sass({
-    //   outputStyle: 'compressed',
-    // }))
-    // .pipe(autoprefixer(CONFIG.autoprefixer))
-    // .pipe(size())
-    // .pipe(gulp.dest(CONFIG.lib))
-
-    // .pipe(concat('zarm.css'))
-    // .pipe(size())
-    // .pipe(gulp.dest(CONFIG.dist))
-    // .pipe(sourcemaps.write())
-    // .pipe(rename('zarm.css.map'))
-    // .pipe(size())
-    // .pipe(gulp.dest(CONFIG.dist))
-
-    // .pipe(cssnano())
-    // .pipe(concat('zarm.min.css'))
-    // .pipe(size())
-    // .pipe(gulp.dest(CONFIG.dist))
-    // .pipe(sourcemaps.write())
-    // .pipe(rename('zarm.min.css.map'))
-    // .pipe(size())
-    // .pipe(gulp.dest(CONFIG.dist));
 });
+
+gulp.task('dist', () => {
+  return gulp.src(CONFIG.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed',
+    }))
+    .pipe(autoprefixer(CONFIG.autoprefixer))
+    .pipe(concat('zarm.css'))
+    .pipe(size())
+    .pipe(gulp.dest(CONFIG.dist))
+    .pipe(sourcemaps.write())
+    .pipe(rename('zarm.css.map'))
+    .pipe(size())
+    .pipe(gulp.dest(CONFIG.dist))
+
+    .pipe(cssnano())
+    .pipe(concat('zarm.min.css'))
+    .pipe(size())
+    .pipe(gulp.dest(CONFIG.dist))
+    .pipe(sourcemaps.write())
+    .pipe(rename('zarm.min.css.map'))
+    .pipe(size())
+    .pipe(gulp.dest(CONFIG.dist));
+});
+
+gulp.task('default', ['copySass', 'dist']);
