5fedbdf22bce8fe0adcbf773cfb04ea0913f8b71
Jerome Lin <jerome.lin@zhongan.com>
2017-11-17 16:47:57 +0800

    jest script

--- scripts/jest/setup.js
+++ scripts/jest/setup.js
@@ -1,32 +1,3 @@
-(() => {
-  if (typeof window === 'undefined') return;
-
-  let lastTime = 0;
-  const vendors = ['ms', 'moz', 'webkit', 'o'];
-  for (let x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-    window.requestAnimationFrame = window[`${vendors[x]}RequestAnimationFrame`];
-    window.cancelAnimationFrame = window[`${vendors[x]}CancelAnimationFrame`] || window[`${vendors[x]}CancelRequestAnimationFrame`];
-  }
-
-  if (!window.requestAnimationFrame) {
-    window.requestAnimationFrame = (callback) => {
-      const currTime = new Date().getTime();
-      const timeToCall = Math.max(0, 16 - (currTime - lastTime));
-      const id = window.setTimeout(() => {
-        callback(currTime + timeToCall);
-      }, timeToCall);
-      lastTime = currTime + timeToCall;
-      return id;
-    };
-  }
-
-  if (!window.cancelAnimationFrame) {
-    window.cancelAnimationFrame = (id) => {
-      clearTimeout(id);
-    };
-  }
-})();
-
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
