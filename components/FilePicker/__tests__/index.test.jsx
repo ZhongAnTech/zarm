3f237d388ecbb5c2a9cd7777cc09f4777bcaa564
Jerome Lin <jerome.lin@zhongan.com>
2017-12-12 13:23:09 +0800

    tests

--- components/FilePicker/__tests__/index.test.jsx
+++ components/FilePicker/__tests__/index.test.jsx
@@ -6,25 +6,11 @@ import FilePicker from '../index';
 describe('FilePicker', () => {
   it('renders correctly', () => {
     const props = {
-      accept: 'image/jpg, image/jpeg, image/gif, image/png',
       onChange: jest.fn(),
-      children: 'foo',
+      children: <div>add</div>,
     };
     const wrapper = render(<FilePicker {...props} />);
     expect(toJson(wrapper)).toMatchSnapshot();
-  });
-
-  it('handle default click', () => {
-    const onClickfn = jest.fn();
-    const props = {
-      accept: 'image/*',
-      onClick: onClickfn,
-      children: 'foo',
-    };
-    const wrapper = mount(<FilePicker {...props} />);
-    wrapper.find('.za-filepicker-input').simulate('click');
-    wrapper.find('.za-filepicker-trigger').simulate('click');
-    // expect(onClickfn).toBeCalled();
   });
 
   it('select file', () => {
@@ -33,7 +19,7 @@ describe('FilePicker', () => {
       onChange: jest.fn(),
       multiple: true,
       onBeforeSelect: jest.fn(),
-      children: <button>+</button>,
+      children: <div>add</div>,
     };
     const wrapper = mount(<FilePicker {...props} />);
     wrapper.find('.za-filepicker-input').simulate('change');
