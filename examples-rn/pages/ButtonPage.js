3314a5ae640a1ebe9c3709184fd6419b899ed8a2
Jerome Lin <jerome.lin@zhongan.com>
2018-01-15 13:32:41 +0800

    button demo

--- examples-rn/pages/ButtonPage.js
+++ examples-rn/pages/ButtonPage.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import { ScrollView, View } from 'react-native';
 import { Button } from '../../components/index.native';
 
-const mb = {
-  marginBottom: 10,
+const styles = {
+  mb: {
+    marginBottom: 10,
+  },
+  mr: {
+    marginRight: 10,
+  },
+  inline: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
 };
-const mr = {
-  marginRight: 10,
-};
-const inline = {
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-};
+
 const noop = () => {};
 
 export default class Page extends Component<{}> {
@@ -19,47 +22,47 @@ export default class Page extends Component<{}> {
     return (
       <ScrollView>
         <View style={{ padding: 15 }}>
-          <View style={{ ...inline, ...mb }}>
-            <Button style={{ ...mb, ...mr }}>default</Button>
-            <Button style={mb} theme="primary">primary</Button>
+          <View style={[styles.inline, styles.mb]}>
+            <Button style={[styles.mb, styles.mr]}>default</Button>
+            <Button style={styles.mb} theme="primary">primary</Button>
           </View>
 
-          <View style={mb}>
-            <Button style={mb} onClick={noop}>default</Button>
-            <Button style={mb} disabled onClick={noop}>default</Button>
-            <Button style={mb} theme="primary" onClick={noop}>primary</Button>
-            <Button style={mb} disabled theme="primary" onClick={noop}>primary</Button>
+          <View style={styles.mb}>
+            <Button style={styles.mb} onClick={noop}>default</Button>
+            <Button style={styles.mb} disabled onClick={noop}>default</Button>
+            <Button style={styles.mb} theme="primary" onClick={noop}>primary</Button>
+            <Button style={styles.mb} disabled theme="primary" onClick={noop}>primary</Button>
           </View>
 
-          <View style={mb}>
-            <Button style={mb} bordered onClick={noop}>default</Button>
-            <Button style={mb} bordered disabled onClick={noop}>disabled default</Button>
-            <Button style={mb} bordered theme="primary" onClick={noop}>primary</Button>
-            <Button style={mb} bordered disabled theme="primary" onClick={noop}>disabled primary</Button>
+          <View style={styles.mb}>
+            <Button style={styles.mb} bordered onClick={noop}>default</Button>
+            <Button style={styles.mb} bordered disabled onClick={noop}>disabled default</Button>
+            <Button style={styles.mb} bordered theme="primary" onClick={noop}>primary</Button>
+            <Button style={styles.mb} bordered disabled theme="primary" onClick={noop}>disabled primary</Button>
           </View>
 
-          <View style={{ ...inline, ...mb }}>
-            <Button style={{ ...mb, ...mr }} onClick={noop}>default</Button>
-            <Button style={{ ...mb, ...mr }} theme="primary" onClick={noop}>primary</Button>
-            <Button style={{ ...mb, ...mr }} theme="success" onClick={noop}>success</Button>
-            <Button style={{ ...mb, ...mr }} theme="warning" onClick={noop}>warning</Button>
-            <Button style={{ ...mb, ...mr }} theme="error" onClick={noop}>error</Button>
+          <View style={[styles.inline, styles.mb]}>
+            <Button style={[styles.mb, styles.mr]} onClick={noop}>default</Button>
+            <Button style={[styles.mb, styles.mr]} theme="primary" onClick={noop}>primary</Button>
+            <Button style={[styles.mb, styles.mr]} theme="success" onClick={noop}>success</Button>
+            <Button style={[styles.mb, styles.mr]} theme="warning" onClick={noop}>warning</Button>
+            <Button style={[styles.mb, styles.mr]} theme="error" onClick={noop}>error</Button>
           </View>
 
-          <View style={{ ...inline, ...mb }}>
-            <Button style={{ ...mb, ...mr }} size="lg" onClick={noop}>lg</Button>
-            <Button style={{ ...mb, ...mr }} onClick={noop}>md</Button>
+          <View style={[styles.inline, styles.mb]}>
+            <Button style={[styles.mb, styles.mr]} size="lg" onClick={noop}>lg</Button>
+            <Button style={[styles.mb, styles.mr]} onClick={noop}>md</Button>
             <Button size="sm" onClick={noop}>sm</Button>
           </View>
 
-          <View style={{ ...inline, ...mb }}>
-            <Button style={{ ...mb, ...mr }} shape="radius" theme="primary" onClick={noop}>radius shape</Button>
-            <Button style={{ ...mb, ...mr }} shape="round" theme="primary" onClick={noop}>round shape</Button>
-            <Button style={{ ...mb, ...mr }} bordered shape="circle" theme="primary" onClick={noop}>Go</Button>
+          <View style={[styles.inline, styles.mb]}>
+            <Button style={[styles.mb, styles.mr]} shape="radius" theme="primary" onClick={noop}>radius shape</Button>
+            <Button style={[styles.mb, styles.mr]} shape="round" theme="primary" onClick={noop}>round shape</Button>
+            <Button style={[styles.mb, styles.mr]} bordered shape="circle" theme="primary" onClick={noop}>Go</Button>
             <Button shape="circle" theme="primary" onClick={noop}>Go</Button>
           </View>
-          <View style={inline}>
-            <Button style={{ ...mb, ...mr }} bordered loading shape="radius" onClick={noop}>loading</Button>
+          <View style={styles.inline}>
+            <Button style={[styles.mb, styles.mr]} bordered loading shape="radius" onClick={noop}>loading</Button>
           </View>
         </View>
       </ScrollView>
