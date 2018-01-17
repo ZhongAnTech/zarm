76db97d874f9a91adc0c5ff2d6f54c26e43b59d7
Jerome Lin <jerome.lin@zhongan.com>
2018-01-17 16:57:47 +0800

    Picker docs

--- components/Picker/PropsType.tsx
+++ components/Picker/PropsType.tsx
@@ -14,8 +14,8 @@ export interface BasePickerViewProps {
 export interface BasePickerProps extends BasePickerViewProps {
   visible?: boolean;
   title?: string;
-  cancelText?: string;
   okText?: string;
+  cancelText?: string;
   onOk?: (value?: object) => void;
   onCancel?: () => void;
   onMaskClick?: () => void;
