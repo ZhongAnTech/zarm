b560014dc51232d8639d4928e187e0075a77c75b
edison-hm <magichuangmian@sina.com>
2018-02-27 20:20:04 +0800

    refs test snap

--- components/DatePickerView/__tests__/index.test.jsx
+++ components/DatePickerView/__tests__/index.test.jsx
@@ -8,18 +8,18 @@ import enLocale from '../locale/en_US';
 
 describe('DatePickerView', () => {
   it('DatePickerView time', () => {
-    // moment.tz.setDefault('Asia/Shanghai');
+    moment.tz.setDefault('Asia/Shanghai');
     const wrapper = mount(
       <DatePickerView
         title="选择时间"
         placeholder="请选择时间"
         mode="datetime"
         // defaultValue={moment('2017-11-03T15:00:00.000Z')}
-        defaultValue="2017-11-03 15:00Z"
+        defaultValue={new Date(moment.tz('2017-11-03 15:00', 'Asia/Shanghai').format())}
         />
     );
     // wrapper.setProps({ defaultValue: '2017-09-06T12:00:00.000Z' });
-    wrapper.setProps({ defaultValue: '2017-09-06 12:00Z' });
+    wrapper.setProps({ defaultValue: new Date(moment.tz('2017-09-06 12:00', 'Asia/Shanghai').format()) });
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
