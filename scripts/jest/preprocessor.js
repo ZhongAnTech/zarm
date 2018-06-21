00ed60b2eec19478752b2d1ec551a0fe955966e6
Jerome Lin <jerome.lin@zhongan.com>
2018-06-21 20:02:49 +0800

    chore: move babel config for test

--- scripts/jest/preprocessor.js
+++ scripts/jest/preprocessor.js
@@ -1,4 +1,6 @@
 const babelJest = require('babel-jest');
 const babelConfig = require('../config/babelConfig');
 
+babelConfig.plugins.push('transform-es2015-modules-commonjs');
+
 module.exports = babelJest.createTransformer(babelConfig);
