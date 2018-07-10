277ef13d8978537af1de6990d82f304897d4ea45
Jerome Lin <jerome.lin@zhongan.com>
2018-07-10 13:20:41 +0800

    chore: examples for button

--- examples/pages/a.jsx
+++
@@ -1,7 +0,0 @@
-import Markdown from '../components/markdown';
-
-export default class Button extends Markdown {
-  document = () => {
-    return require('../../docs/zh-cn/components/a.md');
-  }
-}
