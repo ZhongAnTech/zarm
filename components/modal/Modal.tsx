9cfd368f5f500e48615279612dd82f6b7c12738a
Jerome Lin <jerome.lin@zhongan.com>
2018-07-16 14:31:22 +0800

    fix(modal): modal element have error class when in animation

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -96,7 +96,6 @@ export default class Modal extends PureComponent<ModalProps, any> {
       }),
       dialog: classnames(`${prefixCls}-dialog`, {
         [`${animationType}-${animationState}`]: isPending,
-        [`fade-${animationState}`]: isPending,
       }),
       // mask: classnames({
       //   [`fade-${animationState}`]: isPending,
