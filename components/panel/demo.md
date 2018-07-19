8ff593074239fa575aeb7e6a6bb483ede1f08e56
Jerome Lin <jerome.lin@zhongan.com>
2018-07-19 18:42:47 +0800

    chore(panel): panel api has break change

--- components/panel/demo.md
+++ components/panel/demo.md
@@ -8,12 +8,11 @@ class Demo extends React.Component {
   render() {
     return (
       <div>
-        <Panel>
-          <Panel.Header title="标题" more={<a href="/#" onClick={() => alert('click more')}>更多</a>} />
-          <Panel.Body>
-            <div className="box">内容</div>
-          </Panel.Body>
-          <Panel.Footer title="底部左侧" more="底部右侧" />
+        <Panel
+          titleRender="标题"
+          moreRender={<a href="/#" onClick={() => alert('click more')}>更多</a>}
+        >
+          <div className="box">内容</div>
         </Panel>
       </div>
     )
@@ -30,26 +29,9 @@ ReactDOM.render(<Demo />, mountNode);
 #### Panel
 | 属性 | 类型 | 默认值 | 可选值／参数 | 说明 |
 | :--- | :--- | :--- | :--- | :--- |
+| titleRender | React.ReactNode | | | 标题渲染 |
+| moreRender | React.ReactNode | | | 更多渲染 |
 | prefixCls | string | za-panel | | 类名前缀 |
 | className | string | | | 追加类名 |
-| children | React.element | | | 内容 |
-
-
-#### Panel.Header
-| 属性 | 类型 | 默认值 | 可选值／参数 | 说明 |
-| :--- | :--- | :--- | :--- | :--- |
-| prefixCls | string | za-panel | | 类名前缀 |
-| className | string | | | 追加类名 |
-| title | React.element | | | 标题 |
-| more | React.element | | | 更多 |
-
-
-#### Panel.Footer
-| 属性 | 类型 | 默认值 | 可选值／参数 | 说明 |
-| :--- | :--- | :--- | :--- | :--- |
-| prefixCls | string | za-panel | | 类名前缀 |
-| className | string | | | 追加类名 |
-| title | React.element | | | 标题 |
-| more | React.element | | | 更多 |
 
 :::
--- examples/pages/Index.jsx
+++ examples/pages/Index.jsx
@@ -18,48 +18,39 @@ class Page extends PureComponent {
           </section>
         </header>
         <main>
-          <Panel>
-            <Panel.Header title="表单组件" />
-            <Panel.Body>
-              <Cell hasArrow title="复选框 Checkbox" onClick={() => history.push('/checkbox')} />
-              <Cell hasArrow title="文件选择器 FilePicker" onClick={() => history.push('/file-picker')} />
-              <Cell hasArrow title="文本框 Input" onClick={() => history.push('/input')} />
-              <Cell hasArrow title="选择器 Picker & Select" onClick={() => history.push('/picker')} />
-              <Cell hasArrow title="日期选择器 DatePicker & DateSelect" onClick={() => history.push('/datePicker')} />
-              <Cell hasArrow title="单选框 Radio" onClick={() => history.push('/radio')} />
-              <Cell hasArrow title="滑动输入条 Slider" onClick={() => history.push('/slider')} />
-              <Cell hasArrow title="步进器 Stepper" onClick={() => history.push('/stepper')} />
-              <Cell hasArrow title="开关 Switch" onClick={() => history.push('/switch')} />
-              <Cell hasArrow title="搜索栏 SearchBar" onClick={() => history.push('/searchbar')} />
-            </Panel.Body>
+          <Panel titleRender="表单组件">
+            <Cell hasArrow title="复选框 Checkbox" onClick={() => history.push('/checkbox')} />
+            <Cell hasArrow title="文件选择器 FilePicker" onClick={() => history.push('/file-picker')} />
+            <Cell hasArrow title="文本框 Input" onClick={() => history.push('/input')} />
+            <Cell hasArrow title="选择器 Picker & Select" onClick={() => history.push('/picker')} />
+            <Cell hasArrow title="日期选择器 DatePicker & DateSelect" onClick={() => history.push('/datePicker')} />
+            <Cell hasArrow title="单选框 Radio" onClick={() => history.push('/radio')} />
+            <Cell hasArrow title="滑动输入条 Slider" onClick={() => history.push('/slider')} />
+            <Cell hasArrow title="步进器 Stepper" onClick={() => history.push('/stepper')} />
+            <Cell hasArrow title="开关 Switch" onClick={() => history.push('/switch')} />
+            <Cell hasArrow title="搜索栏 SearchBar" onClick={() => history.push('/searchbar')} />
           </Panel>
-          <Panel>
-            <Panel.Header title="操作反馈" />
-            <Panel.Body>
-              <Cell hasArrow title="动作面板 ActionSheet" onClick={() => history.push('/action-sheet')} />
-              <Cell hasArrow title="按钮 Button" onClick={() => history.push('/button')} />
-              <Cell hasArrow title="模态框 Modal" onClick={() => history.push('/modal')} />
-              <Cell hasArrow title="弹出框 Popup" onClick={() => history.push('/popup')} />
-              <Cell hasArrow title="上拉加载下拉刷新 Pull" onClick={() => history.push('/pull')} />
-              <Cell hasArrow title="滑动操作 SwipeAction" onClick={() => history.push('/swipe-action')} />
-              <Cell hasArrow title="轻提示 Toast" onClick={() => history.push('/toast')} />
-            </Panel.Body>
+          <Panel titleRender="操作反馈">
+            <Cell hasArrow title="动作面板 ActionSheet" onClick={() => history.push('/action-sheet')} />
+            <Cell hasArrow title="按钮 Button" onClick={() => history.push('/button')} />
+            <Cell hasArrow title="模态框 Modal" onClick={() => history.push('/modal')} />
+            <Cell hasArrow title="弹出框 Popup" onClick={() => history.push('/popup')} />
+            <Cell hasArrow title="上拉加载下拉刷新 Pull" onClick={() => history.push('/pull')} />
+            <Cell hasArrow title="滑动操作 SwipeAction" onClick={() => history.push('/swipe-action')} />
+            <Cell hasArrow title="轻提示 Toast" onClick={() => history.push('/toast')} />
           </Panel>
-          <Panel>
-            <Panel.Header title="数据展示" />
-            <Panel.Body>
-              <Cell hasArrow title="折叠面板 Collapse" onClick={() => history.push('/collapse')} />
-              <Cell hasArrow title="徽标 Badge" onClick={() => history.push('/badge')} />
-              <Cell hasArrow title="列表项 Cell" onClick={() => history.push('/cell')} />
-              <Cell hasArrow title="图标 Icon" onClick={() => history.push('/icon')} />
-              <Cell hasArrow title="消息 Message" onClick={() => history.push('/message')} />
-              <Cell hasArrow title="通告栏 NoticeBar" onClick={() => history.push('/notice-bar')} />
-              <Cell hasArrow title="面板 Panel" onClick={() => history.push('/panel')} />
-              <Cell hasArrow title="进度条 Progress" onClick={() => history.push('/progress')} />
-              <Cell hasArrow title="指示器 Spinner" onClick={() => history.push('/spinner')} />
-              <Cell hasArrow title="图片轮播 Swipe" onClick={() => history.push('/swipe')} />
-              <Cell hasArrow title="标签页 Tab" onClick={() => history.push('/tab')} />
-            </Panel.Body>
+          <Panel titleRender="数据展示">
+            <Cell hasArrow title="折叠面板 Collapse" onClick={() => history.push('/collapse')} />
+            <Cell hasArrow title="徽标 Badge" onClick={() => history.push('/badge')} />
+            <Cell hasArrow title="列表项 Cell" onClick={() => history.push('/cell')} />
+            <Cell hasArrow title="图标 Icon" onClick={() => history.push('/icon')} />
+            <Cell hasArrow title="消息 Message" onClick={() => history.push('/message')} />
+            <Cell hasArrow title="通告栏 NoticeBar" onClick={() => history.push('/notice-bar')} />
+            <Cell hasArrow title="面板 Panel" onClick={() => history.push('/panel')} />
+            <Cell hasArrow title="进度条 Progress" onClick={() => history.push('/progress')} />
+            <Cell hasArrow title="指示器 Spinner" onClick={() => history.push('/spinner')} />
+            <Cell hasArrow title="图片轮播 Swipe" onClick={() => history.push('/swipe')} />
+            <Cell hasArrow title="标签页 Tab" onClick={() => history.push('/tab')} />
           </Panel>
         </main>
         <Footer />
