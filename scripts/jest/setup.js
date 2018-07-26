0091394ce895a9b1e3d7ddedb03d6c2affa549c7
edison-hm <magichuangmian@sina.com>
2018-07-26 19:29:50 +0800

    docs: add test rn and remove useless package

--- scripts/jest/setup.js
+++ scripts/jest/setup.js
@@ -3,7 +3,5 @@ const Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
 
-require('react-native-mock-render/mock');
-
 jest.mock('react-native', () => require('react-native-mock-render'), { virtual: true });
 
