0110e5726da93f2896e1f928b476352cf8da2e2c
JasonYou <qingwei0713@163.com>
2018-08-19 17:54:30 +0800

    chore: merge dev

--- components/cell/index.native.tsx
+++ components/cell/index.native.tsx
@@ -12,103 +12,104 @@ import { BasePanelProps } from './PropsType';
 import cellStyle from './style/index.native';
 
 export interface CellProps extends BasePanelProps {
-    style?: CSSProperties;
-    styles?: typeof cellStyle;
+  style?: CSSProperties;
+  styles?: typeof cellStyle;
 }
 
 const cellStyles = StyleSheet.create<any>(cellStyle);
 
 export default class Cell extends PureComponent<CellProps, any> {
-    static defaultProps = {
-        active: false,
-        styles: cellStyles,
-        onClick() {},
+  static defaultProps = {
+    active: false,
+    styles: cellStyles,
+    onClick() {},
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: props.active,
     };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isActive: props.active,
-        };
+  componentWillReceiveProps(nextProps) {
+    if ('active' in nextProps) {
+      this.setState({ isActive: nextProps.active });
     }
+  }
 
-    componentWillReceiveProps(nextProps) {
-        if ('active' in nextProps) {
-          this.setState({ isActive: nextProps.active });
-        }
-    }
+  onPressIn = () => {
+    this.setState({ isActive: true });
+  }
 
-    onPressIn = () => {
-        this.setState({ isActive: true });
-    }
+  onPressOut = () => {
+    this.setState({ isActive: false });
+  }
 
-    onPressOut = () => {
-        this.setState({ isActive: false });
-    }
+  render() {
+    const {
+      theme,
+      hasArrow,
+      icon,
+      title,
+      description,
+      help,
+      disabled,
+      style,
+      styles,
+      onClick,
+      children,
+      ...others
+    } = this.props;
+    const { isActive } = this.state;
 
-    render() {
-        const {
-            theme,
-            hasArrow,
-            icon,
-            title,
-            description,
-            help,
-            disabled,
-            style,
-            styles,
-            onClick,
-            children,
-            ...others,
-        } = this.props;
-        const { isActive } = this.state;
+    const wrapperStyle = [
+      styles.wrapperStyle,
+      style,
+    ];
+    const containerStyle = [
+      styles.containerStyle,
+    ];
+    const bodyStyle = [
+      styles.bodyStyle,
+      description ? styles.flexDirectionColumn : styles.flexDirectionRow,
+      description ? styles.alignItemsStart : styles.alignItemsCenter,
+    ];
+    const iconStyle = [
+      styles.iconStyle,
+    ];
+    const titleStyle = [
+      description && styles.paddingBottom,
+    ];
+    const arrowStyle = [
+      styles.arrowStyle,
+    ];
+    const helpStyle = [
+      styles.helpStyle,
+    ];
+    const underlayColor = StyleSheet.flatten(styles.underlayColorStyle).backgroundColor;
 
-        const wrapperStyle = [
-            styles.wrapperStyle,
-            style
-        ];
-        const containerStyle = [
-            styles.containerStyle
-        ];
-        const bodyStyle = [
-            styles.bodyStyle,
-            description ? styles.flexDirectionColumn : styles.flexDirectionRow,
-            description ? styles.alignItemsStart : styles.alignItemsCenter
-        ];
-        const iconStyle = [
-            styles.iconStyle,
-        ];
-        const titleStyle = [
-            description && styles.paddingBottom
-        ];
-        const arrowStyle = [
-            styles.arrowStyle
-        ];
-        const helpStyle = [
-            styles.helpStyle
-        ];
-        const underlayColor = StyleSheet.flatten(styles.underlayColorStyle).backgroundColor;
-
-        const iconRender = icon && <View style={iconStyle}>{icon}</View>;
-        const titleRender = title && <View style={titleStyle}>{title}</View>;
-        const descriptionRender = description && <View>{description}</View>;
-        const arrowRender = hasArrow && <View style={arrowStyle}/>;
-        const helpRender = help && <View style={helpStyle}>{help}</View>;
-        const contentRender = <View style={!hasArrow && wrapperStyle}>
-            <View style={containerStyle}>
-                {iconRender}
-                <View style={bodyStyle}>
-                    {titleRender}
-                    {children}
-                </View>
-                {descriptionRender}
-                {arrowRender}
-            </View>
-            {helpRender}
+    const iconRender = icon && <View style={iconStyle}>{icon}</View>;
+    const titleRender = title && <View style={titleStyle}>{title}</View>;
+    const descriptionRender = description && <View>{description}</View>;
+    const arrowRender = hasArrow && <View style={arrowStyle}/>;
+    const helpRender = help && <View style={helpStyle}>{help}</View>;
+    const contentRender = <View style={!hasArrow && wrapperStyle}>
+      <View style={containerStyle}>
+        {iconRender}
+        <View style={bodyStyle}>
+          {titleRender}
+          {children}
         </View>
-        const wrapperProps = Object.assign({ activeOpacity: 0.3, style: wrapperStyle, onPress: onClick, onPressIn: this.onPressIn, onPressOut: this.onPressOut }, others);
-        return hasArrow
-            ? <TouchableHighlight {...wrapperProps} underlayColor={underlayColor}>{contentRender}</TouchableHighlight>
-            : (contentRender);
-    }
+        {descriptionRender}
+        {arrowRender}
+      </View>
+      {helpRender}
+    </View>;
+    const wrapperProps = Object.assign({ activeOpacity: 0.3, style: wrapperStyle, onPress: onClick,
+      onPressIn: this.onPressIn, onPressOut: this.onPressOut }, others);
+    return hasArrow
+      ? <TouchableHighlight {...wrapperProps} underlayColor={underlayColor}>{contentRender}</TouchableHighlight>
+      : (contentRender);
+  }
 }
--- examples-rn/pages/CellPage.js
+++ examples-rn/pages/CellPage.js
@@ -1,32 +1,32 @@
 import React, { PureComponent } from 'react';
-import { ScrollView, View, Text, TouchableHighlight, Image } from 'react-native';
+import { ScrollView, View, Text, Image } from 'react-native';
 import { Cell } from '../../components/index.native';
 
 const styles = {
   box: {
-      marginBottom: 10,
-      flexDirection: 'row',
-      flexWrap: 'wrap',
+    marginBottom: 10,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
   cellStyle: {
-      // minHeight: 52,
+    // minHeight: 52,
   },
   iconStyle: {
-      width: 24,
-      height: 24,
-      backgroundColor: '#3385ff',
+    width: 24,
+    height: 24,
+    backgroundColor: '#3385ff',
   },
   titleTextStyle: {
-      color: '#464646',
-      fontSize: 15,
+    color: '#464646',
+    fontSize: 15,
   },
   descriptionTextStyle: {
-      color: '#909090',
-      fontSize: 15,
+    color: '#909090',
+    fontSize: 15,
   },
   helpTextStyle: {
-      color: '#FF5050',
-      fontSize: 10,
+    color: '#FF5050',
+    fontSize: 10,
   },
 };
 export default class App extends PureComponent {
@@ -36,65 +36,71 @@ export default class App extends PureComponent {
         <View style={{ padding: 15 }}>
           <View style={styles.box}>
             <Cell
-                style={styles.cellStyle}
-                title={<Text style={styles.titleTextStyle}>标题</Text>}>
-                <View>
-                    <Text>我是内容</Text>
-                </View>
+              style={styles.cellStyle}
+              title={<Text style={styles.titleTextStyle}>标题</Text>}
+            >
+              <View>
+                <Text>我是内容</Text>
+              </View>
             </Cell>
           </View>
           <View style={styles.box}>
             <Cell
-                style={styles.cellStyle}
-                title={<Text style={styles.titleTextStyle}>标题</Text>}
-                description={<Text style={styles.descriptionTextStyle}>我是描述</Text>}>
-                <View>
-                    <Text>我是内容</Text>
-                </View>
+              style={styles.cellStyle}
+              title={<Text style={styles.titleTextStyle}>标题</Text>}
+              description={<Text style={styles.descriptionTextStyle}>我是描述</Text>}
+            >
+              <View>
+                <Text>我是内容</Text>
+              </View>
             </Cell>
           </View>
           <View style={styles.box}>
             <Cell
-                style={styles.cellStyle}
-                title={<Text style={styles.titleTextStyle}>标题</Text>}
-                description={<Text style={styles.descriptionTextStyle}>我是描述</Text>}
-                hasArrow>
-                <View>
-                    <Text>我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容</Text>
-                </View>
+              style={styles.cellStyle}
+              title={<Text style={styles.titleTextStyle}>标题</Text>}
+              description={<Text style={styles.descriptionTextStyle}>我是描述</Text>}
+              hasArrow
+            >
+              <View>
+                <Text>我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容我是内容</Text>
+              </View>
             </Cell>
           </View>
           <View style={styles.box}>
             <Cell
-                style={styles.cellStyle}
-                icon={<Image style={styles.iconStyle} source={{uri: 'https://static.zhongan.com/website/open/assets/wp/new_portal/1.2.8/img/h1_i5.97dafb6.png'}}></Image>}
-                title={<Text style={styles.titleTextStyle}>标题</Text>}>
-                <View>
-                    <Text>我是内容</Text>
-                </View>
+              style={styles.cellStyle}
+              icon={<Image style={styles.iconStyle} source={{ uri: 'https://static.zhongan.com/website/open/assets/wp/new_portal/1.2.8/img/h1_i5.97dafb6.png' }} />}
+              title={<Text style={styles.titleTextStyle}>标题</Text>}
+            >
+              <View>
+                <Text>我是内容</Text>
+              </View>
             </Cell>
           </View>
           <View style={styles.box}>
             <Cell
-                style={styles.cellStyle}
-                title={<Text style={styles.titleTextStyle}>标题</Text>}
-                description={<Text style={styles.descriptionTextStyle}>我是描述</Text>}
-                hasArrow>
-                <View>
-                    <Text>我是内容</Text>
-                </View>
+              style={styles.cellStyle}
+              title={<Text style={styles.titleTextStyle}>标题</Text>}
+              description={<Text style={styles.descriptionTextStyle}>我是描述</Text>}
+              hasArrow
+            >
+              <View>
+                <Text>我是内容</Text>
+              </View>
             </Cell>
           </View>
           <View style={styles.box}>
             <Cell
-                style={styles.cellStyle}
-                title={<Text style={styles.titleTextStyle}>标题</Text>}
-                description={<Text style={styles.descriptionTextStyle}>我是描述</Text>}
-                hasArrow
-                help={<View><Text style={styles.helpTextStyle}>我是help</Text></View>}>
-                <View>
-                    <Text>我是内容</Text>
-                </View>
+              style={styles.cellStyle}
+              title={<Text style={styles.titleTextStyle}>标题</Text>}
+              description={<Text style={styles.descriptionTextStyle}>我是描述</Text>}
+              hasArrow
+              help={<View><Text style={styles.helpTextStyle}>我是help</Text></View>}
+            >
+              <View>
+                <Text>我是内容</Text>
+              </View>
             </Cell>
           </View>
         </View>
