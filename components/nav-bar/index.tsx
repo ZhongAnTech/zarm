b8a406c5ae50e90f32bd397af64feab2e3a54a52
TianGong <397009765@qq.com>
2018-09-14 18:38:16 +0800

    fix: solve icon bugs

--- components/nav-bar/index.tsx
+++ components/nav-bar/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import BaseNavbarProps from './PropsType';
+import Icon from '../icon';
+import classnames from 'classnames';
 
 export interface NavBarProps extends BaseNavbarProps {
   prefixCls?: string;
@@ -12,14 +14,42 @@ export default class NavBar extends React.PureComponent<NavBarProps> {
   };
 
   render() {
-    const { style, title, left, right } = this.props;
+    const { prefixCls, className, style, title, left, right } = this.props;
+    const cls = classnames(`${prefixCls}`, className);
+    const titleCls = `${prefixCls}-title`;
+    const iconCls = `${prefixCls}-icon`;
+    const leftCls = `${prefixCls}-left`;
+    const leftClsIcon = classnames(`${prefixCls}-left-icon`, iconCls);
+    const rightCls = `${prefixCls}-right`;
 
-    return (
-      <div style={style}>
-        {left}
-        {title}
-        {right}
-      </div>
-    );
+    const renderLeft = () => {
+      if (left && typeof left === 'boolean') {
+        return (
+          <Icon className={leftClsIcon} theme="success" type="arrow-left" />
+        );
+      } else if (!!left) {
+        return (
+          <div className={leftCls}>
+            {left}
+          </div>
+        );
+      }
+    };
+
+    const renderRight = () => {
+      if (right && typeof right !== 'boolean') {
+        return (
+          <div className={rightCls}>
+            {right}
+          </div>
+        );
+      }
+    };
+
+    return <div className={cls} style={style}>
+        {renderLeft()}
+        <div className={titleCls}>{title}</div>
+        {renderRight()}
+      </div>;
   }
 }
