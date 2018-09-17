5f9160ffb49b50e0cef24fc120ef8aaf0a1f578c
TianGong <397009765@qq.com>
2018-09-17 14:28:31 +0800

    fix: comment some code due to propstype conflicts

--- components/nav-bar/PropsType.tsx
+++ components/nav-bar/PropsType.tsx
@@ -1,9 +1,4 @@
 export default interface BaseNavbarProps {
-  showRight?: boolean;
-  leftText?: string;
-  rightText?: string;
-  onClickLeft?: () => void;
-  onClickRight?: () => void;
   title?: React.ReactNode;
   left?: React.ReactNode;
   right?: React.ReactNode;
