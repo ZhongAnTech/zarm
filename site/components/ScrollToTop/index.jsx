390ae5708e4b8a55669056c44f3874d47a075c29
Jerome Lin <jerome.lin@zhongan.com>
2018-10-10 18:41:33 +0800

    chore: add click event for scroll-to-top

--- site/components/ScrollToTop/index.jsx
+++ site/components/ScrollToTop/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { Icon } from 'dragon-ui';
 import Events from '@site/utils/events';
@@ -17,18 +18,20 @@ class ScrollToTop extends Component {
 
   componentDidMount() {
     document.body.appendChild(this.container);
-    Events.on(document, 'scroll', this.throttledScroll);
+    Events.on(window, 'scroll', this.throttledScroll);
   }
 
   componentWillUnmount() {
-    Events.off(document, 'scroll', this.throttledScroll);
+    Events.off(window, 'scroll', this.throttledScroll);
   }
 
   onScroll = () => {
     this.setState({
-      visible: document.documentElement.scrollTop > 300,
+      visible: this.getScrollTop() > this.props.visibleDistance,
     });
   }
+
+  getScrollTop = () => document.documentElement.scrollTop || document.body.scrollTop;
 
   getContainer() {
     if (!this.container) {
@@ -39,20 +42,48 @@ class ScrollToTop extends Component {
     return this.container;
   }
 
+  scrollToTop = () => {
+    const timer = setInterval(() => {
+      let st = this.getScrollTop();
+      st -= this.props.speed;
+      if (st > 0) {
+        window.scrollTo(0, st);
+      } else {
+        window.scrollTo(0, 0);
+        clearInterval(timer);
+      }
+    }, 10);
+  }
+
   render() {
     const cls = classnames('scroll-to-top', {
       hide: !this.state.visible,
     });
 
     return ReactDOM.createPortal(
-      <div className={cls} onClick={() => {
-        alert(1);
-      }}>
+      <div
+        className={cls}
+        onClick={this.scrollToTop}
+      >
         <Icon type="arrow-top" />
       </div>,
       this.getContainer()
     );
   }
 }
+
+ScrollToTop.propTypes = {
+  // 滚动速度
+  speed: PropTypes.number,
+  // 离滚动条顶部的可视距离
+  visibleDistance: PropTypes.number,
+};
+
+ScrollToTop.defaultProps = {
+  // 每10毫秒滑动的距离
+  speed: 60,
+  // 距离滚动条顶部300像素才出现按钮
+  visibleDistance: 300,
+};
 
 export default ScrollToTop;
