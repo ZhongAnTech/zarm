3ff69b6a4a61d978789303f85f708fe8006b35f2
Jerome Lin <jerome.lin@zhongan.com>
2018-10-17 17:13:38 +0800

    chore: remove unused code

--- site/components/Markdown.jsx
+++ site/components/Markdown.jsx
@@ -1,48 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import marked from 'marked';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/github-gist.css';
 
 export default class Markdown extends React.Component {
-  constructor(props) {
-    super(props);
-    this.components = new Map();
-  }
-
-  componentDidMount() {
-    this.renderDOM();
-  }
-
-  componentDidUpdate() {
-    this.renderDOM();
-  }
-
-  componentWillUnmount() {
-    this.nodeList.forEach((node) => {
-      ReactDOM.unmountComponentAtNode(node);
-    });
-  }
-
-  nodeList = [];
-
-  renderDOM() {
-    // eslint-disable-next-line
-    for (const [id, component] of this.components) {
-      const div = document.getElementById(id);
-      this.nodeList.push(div);
-      if (div instanceof HTMLElement) {
-        ReactDOM.render(component, div);
-      }
-    }
-  }
-
   render() {
     const { document, className } = this.document(localStorage.getItem('LANGUAGE') || 'zh-CN');
 
     if (typeof document === 'string') {
-      this.components.clear();
-
       const renderer = new marked.Renderer();
       renderer.table = (header, body) => {
         return `<div class="grid-container"><table class="grid"><thead>${header}</thead><tbody>${body}</tbody></table></div>`;
