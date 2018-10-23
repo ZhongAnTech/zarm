7bc4306b5ecb89f42c9f935587c0771a3ce563c4
Jerome Lin <jerome.lin@zhongan.com>
2018-10-23 18:49:47 +0800

    chore: optimize marked renderer config

--- site/components/Markdown.jsx
+++ site/components/Markdown.jsx
@@ -9,10 +9,13 @@ export default class Markdown extends Component {
 
     if (typeof document === 'string') {
       const renderer = new marked.Renderer();
+
+      // 表格
       renderer.table = (header, body) => {
         return `<div class="grid-container"><table class="grid"><thead>${header}</thead><tbody>${body}</tbody></table></div>`;
       };
 
+      // 代码
       // highlightjs对jsx解析还不完善，自闭合标签会破坏高亮显示，暂未解决。
       // https://github.com/highlightjs/highlight.js/issues/1646
       renderer.code = (code, language) => {
@@ -23,11 +26,20 @@ export default class Markdown extends Component {
         // Render the highlighted code with `hljs` class.
         return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
       };
-      marked.setOptions({ renderer });
-      const html = marked(document, {
-        renderer,
-      });
 
+      // 标题
+      // renderer.heading = (text, level) => {
+      //   const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+      //   return `
+      //     <h${level}>
+      //       <a name="${escapedText}" class="anchor" href="#${escapedText}">
+      //         <span class="header-link">#</span>
+      //       </a>
+      //       ${text}
+      //     </h${level}>`;
+      // };
+
+      const html = marked(document, { renderer });
       return <div className={className} dangerouslySetInnerHTML={{ __html: html }} />;
     }
 
