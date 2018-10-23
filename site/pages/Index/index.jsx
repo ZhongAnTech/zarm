bb922660e5c9431914d374041421285ec1e026c3
Jerome Lin <jerome.lin@zhongan.com>
2018-10-23 15:38:44 +0800

    chore: update site index page

--- site/pages/Index/index.jsx
+++ site/pages/Index/index.jsx
@@ -1,5 +1,4 @@
 import React, { PureComponent } from 'react';
-import { Cell } from 'zarm';
 import Container from '@site/components/Container';
 import Footer from '@site/components/Footer';
 
@@ -9,9 +8,16 @@ class Page extends PureComponent {
 
     return (
       <Container className="index-page">
-        <main>
-          <Cell hasArrow title="docs" onClick={() => history.push('/docs/input')} />
-        </main>
+        <main
+          style={{
+            margin: '0 auto',
+            width: 1500,
+            height: 1000,
+            background: `url(${require('./images/index.jpg')}) no-repeat top center`,
+            backgroundSize: 'contain',
+          }}
+          onClick={() => history.push('/docs/input')}
+        />
         <Footer />
       </Container>
     );
