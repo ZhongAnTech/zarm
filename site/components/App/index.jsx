bcd132aade064e2b2241e207a30726d93e79b0b2
Jerome Lin <jerome.lin@zhongan.com>
2018-10-24 15:22:01 +0800

    chore: save menu scroll top when change page hash

--- site/components/App/index.jsx
+++ site/components/App/index.jsx
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
-import Loadable from 'react-loadable';
+// import Loadable from 'react-loadable';
 import './style.scss';
 
-const LoadableComponent = (component) => {
-  return Loadable({
-    loader: component,
-    loading: () => null,
-  });
-};
+// const LoadableComponent = (component) => {
+//   return Loadable({
+//     loader: component,
+//     loading: () => null,
+//   });
+// };
 
 class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={LoadableComponent(() => import('@site/pages/Index'))} />
+        {/* <Route exact path="/" component={LoadableComponent(() => import('@site/pages/Index'))} />
         <Route path="/docs" component={LoadableComponent(() => import('@site/pages/Docs'))} />
-        <Route path="*" component={LoadableComponent(() => import('@site/pages/NotFoundPage'))} />
+        <Route path="*" component={LoadableComponent(() => import('@site/pages/NotFoundPage'))} /> */}
+        <Route exact path="/" component={require('@site/pages/Index').default} />
+        <Route path="/docs" component={require('@site/pages/Docs').default} />
+        <Route path="*" component={require('@site/pages/NotFoundPage').default} />
       </Switch>
     );
   }
