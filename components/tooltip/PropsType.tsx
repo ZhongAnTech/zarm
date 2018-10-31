7cd89ae05b7bcfa1029002748b603273b351a35b
develop-zw <wb_zhangwen@zhongan.io>
2018-10-31 16:34:31 +0800

    feat: tooltip

--- components/tooltip/PropsType.tsx
+++ components/tooltip/PropsType.tsx
@@ -1,6 +1,17 @@
 
-export type direction = 'topLeft' | 'top' | 'topRight' | 'rightTop' | 'right' | 'rightBottom' | 'bottomLeft' | 'bottom' | 'bottomRight' | 'leftTop' | 'left' | 'leftBottom';
-
+export type direction =
+  | 'topLeft'
+  | 'top'
+  | 'topRight'
+  | 'rightTop'
+  | 'right'
+  | 'rightBottom'
+  | 'bottomLeft'
+  | 'bottom'
+  | 'bottomRight'
+  | 'leftTop'
+  | 'left'
+  | 'leftBottom';
 export default interface PropsType {
   prefixCls?: string;
   className?: string;
@@ -9,5 +20,5 @@ export default interface PropsType {
   direction: direction;
   onVisibleChange?: (visible: boolean) => void;
   title?: React.ReactNode;
-  style?: React.CSSProperties
+  style?: React.CSSProperties;
 }
--- components/tooltip/index.tsx
+++ components/tooltip/index.tsx
@@ -1,60 +1,203 @@
-import React, { PureComponent, cloneElement } from 'react';
-import ReactDOM, { findDOMNode } from 'react-dom';
+import React, { Component } from 'react';
+import { findDOMNode, createPortal } from 'react-dom';
 import classnames from 'classnames';
+import Popper from './popper';
+import Events from '../utils/events';
 import PropsType from './PropsType';
-// import Events from '../utils/events';
 
-export interface TooltipProps extends PropsType {
-  prefixCls?: string;
-  className?: string;
-}
+const directMap = {
+  top: 'top',
+  topLeft: 'top-start',
+  topRight: 'top-end',
+  right: 'right',
+  rightTop: 'right-start',
+  rightBottom: 'right-end',
+  bottom: 'bottom',
+  bottomLeft: 'bottom-start',
+  bottomRight: 'bottom-end',
+  left: 'left',
+  leftTop: 'left-start',
+  leftBottom: 'left-end',
+};
 
-export default class Tooltip extends PureComponent<TooltipProps, any> {
+class Tootip extends Component<PropsType, any> {
   static defaultProps = {
     prefixCls: 'za-tooltip',
+    className: null,
     visible: false,
+    trigger: 'hover',
+    direction: 'bottomRight',
+    onVisibleChange: () => {},
+    title: null,
   };
 
-  private child;
+  private instance;
+  private pop;
+  private reference;
+  private popper;
+  private timer;
+  private arrow;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: !!props.visible,
+    };
+  }
 
   componentDidMount() {
-    if (!window.zarmTooltip) {
-      window.zarmTooltip = document.createElement('div');
-      document.body.appendChild(window.zarmTooltip);
+    const { instance, pop } = this;
+    const reference = findDOMNode(this.reference);
+    const { trigger } = this.props;
+    if (trigger === 'click') {
+      Events.on(reference, 'click', () => {
+        this.setState({
+          visible: !this.state.visible,
+        });
+      });
+      Events.on(document, 'click', ({ target }) => {
+        if (
+          !instance ||
+          instance.contains(target) ||
+          !reference ||
+          reference.contains(target) ||
+          !pop ||
+          pop.contains(target) ||
+          !this.popper
+        ) {
+          return;
+        }
+        this.hidePop();
+      });
+    } else {
+      Events.on(reference, 'mouseenter', () => {
+        this.showPop();
+      });
+      Events.on(reference, 'mouseleave', () => {
+        this.hidePop();
+      });
+      Events.on(pop, 'mouseenter', () => {
+        this.showPop();
+      });
+      Events.on(pop, 'mouseleave', () => {
+        this.hidePop();
+      });
     }
-    this.show(this.props);
+    this.componentDidUpdate();
   }
 
   componentWillReceiveProps(nextProps) {
-    this.show(nextProps);
+    if (this.state.visible !== nextProps.visible) {
+      this.setState({
+        visible: !!nextProps.visible,
+      });
+    }
   }
 
-  show = (props) => {
-    const { prefixCls, className, visible, message } = props;
-    const cls = classnames(prefixCls, className, {
-      [`${prefixCls}-hidden`]: !visible,
+  componentDidUpdate() {
+    const { visible } = this.state;
+    const { direction, onVisibleChange } = this.props;
+    const reference = findDOMNode(this.reference);
+
+    if (visible) {
+      if (this.popper) {
+        this.popper.update();
+      } else {
+        if (this.arrow) {
+          this.arrow.setAttribute('x-arrow', '');
+        }
+        this.popper = new Popper(reference, this.pop, {
+          placement: directMap[direction],
+        });
+      }
+    } else {
+      if (this.popper) {
+        this.popper.destroy();
+      }
+      delete this.popper;
+    }
+    if (!!onVisibleChange) {
+      onVisibleChange(visible);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.popper) {
+      this.popper.destroy();
+    }
+    delete this.popper;
+  }
+
+  showPop() {
+    clearTimeout(this.timer);
+    this.setState({
+      visible: true,
     });
+  }
 
-    const dom = findDOMNode(this.child) as HTMLElement;
-    const rect = dom.getBoundingClientRect();
-    const scrollTop = document.documentElement.scrollTop + document.body.scrollTop;
-    const style = {
-      left: rect.left,
-      top: rect.top + scrollTop,
-      width: rect.width,
-    };
-
-    ReactDOM.render(
-      <div className={cls} style={style}>
-        <div className={`${prefixCls}-inner`}>{message}</div>
-      </div>
-      , window.zarmTooltip);
+  hidePop() {
+    const { trigger } = this.props;
+    if (trigger === 'click') {
+      this.setState({
+        visible: false,
+      });
+      return;
+    }
+    this.timer = setTimeout(() => {
+      this.setState({
+        visible: false,
+      });
+    }, 200);
   }
 
   render() {
-    const { children } = this.props;
-    return cloneElement(children, {
-      ref: (ele) => { this.child = ele; },
+    const { visible } = this.state;
+    const {
+      children,
+      title,
+      prefixCls,
+      className,
+    } = this.props;
+    const child = React.isValidElement(children) ? (
+      children
+    ) : (
+      <span>{children}</span>
+    );
+    const popContent = typeof title === 'function' ? title() : title;
+    const cls = classnames(prefixCls, {
+      [className!]: !!className,
     });
+    const contentCls = classnames({
+      [`${prefixCls}-content`]: true,
+      [`${prefixCls}-content-show`]: visible,
+    });
+    const dd = () => {
+      return (
+        <div
+          className={contentCls}
+          ref={(pop) => { this.pop = pop; }}
+        >
+          {popContent}
+          <span
+            className={`${prefixCls}-arrow`}
+            ref={(arrow) => { this.arrow = arrow; }}
+          />
+        </div>
+      );
+    };
+    return (
+      <div
+        className={cls}
+        // tslint:disable-next-line:jsx-no-multiline-js
+        ref={(instance) => {
+          this.instance = instance;
+        }}
+      >
+        {createPortal(dd(), document.body)}
+        {React.cloneElement(child, { ref: (reference) => { this.reference = reference; } })}
+      </div>
+    );
   }
 }
+
+export default Tootip;
