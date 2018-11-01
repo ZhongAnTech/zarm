863b5a33a64e3f5ac393b7c82397acbf8d33a80f
Jerome Lin <jerome.lin@zhongan.com>
2018-11-01 13:35:00 +0800

    chore: site update

--- site/components/Header/index.jsx
+++ site/components/Header/index.jsx
@@ -24,51 +24,53 @@ class Header extends PureComponent {
     const { form, feedback, view, navigation } = components;
     return (
       <header>
-        <div className="logo">
-          <a href="#/">Zarm</a>
+        <div className="header-container">
+          <div className="logo">
+            <a href="#/">ZARM</a>
+          </div>
+          <div className="search">
+            <Select
+              radius
+              search
+              placeholder="搜索组件..."
+              value={this.state.searchKey}
+              onChange={(data) => {
+                history.replace(`/components/${data.value}`);
+                this.setState({
+                  searchKey: null,
+                });
+              }}
+            >
+              {
+                [...form, ...feedback, ...view, ...navigation].map((component, i) => {
+                  return <Select.Option key={+i} value={Format.camel2Dash(component.name)}>{`${component.name} ${component.description}`}</Select.Option>;
+                })
+              }
+            </Select>
+          </div>
+          {/* <div className="lang">
+            <Dropdown
+              trigger="hover"
+              visible={this.state.dropdown}
+              onVisibleChange={(visible) => {
+                this.setState({
+                  dropdown: visible,
+                });
+              }}
+              overlay={<canvas ref={(ele) => { this.qrcode = ele; }} />}
+            >
+              <Button theme="info">扫码体验</Button>
+            </Dropdown>
+            <Button>English</Button>
+          </div> */}
+          {/* <nav>
+            <Menu mode="horizontal" defaultSelectedKeys={['components']}>
+              <Menu.Item key="index" onClick={() => history.replace('/')}>首页</Menu.Item>
+              <Menu.Item key="components">组件</Menu.Item>
+              <Menu.Item key="github" onClick={() => window.open('https://github.com/ZhonganTechENG/zarm')}>github</Menu.Item>
+            </Menu>
+          </nav> */}
         </div>
-        <div className="search">
-          <Select
-            radius
-            search
-            placeholder="搜索组件..."
-            value={this.state.searchKey}
-            onChange={(data) => {
-              history.replace(`/components/${data.value}`);
-              this.setState({
-                searchKey: null,
-              });
-            }}
-          >
-            {
-              [...form, ...feedback, ...view, ...navigation].map((component, i) => {
-                return <Select.Option key={+i} value={Format.camel2Dash(component.name)}>{`${component.name} ${component.description}`}</Select.Option>;
-              })
-            }
-          </Select>
-        </div>
-        {/* <div className="lang">
-          <Dropdown
-            trigger="hover"
-            visible={this.state.dropdown}
-            onVisibleChange={(visible) => {
-              this.setState({
-                dropdown: visible,
-              });
-            }}
-            overlay={<canvas ref={(ele) => { this.qrcode = ele; }} />}
-          >
-            <Button theme="info">扫码体验</Button>
-          </Dropdown>
-          <Button>English</Button>
-        </div> */}
-        {/* <nav>
-          <Menu mode="horizontal" defaultSelectedKeys={['components']}>
-            <Menu.Item key="index" onClick={() => history.replace('/')}>首页</Menu.Item>
-            <Menu.Item key="components">组件</Menu.Item>
-            <Menu.Item key="github" onClick={() => window.open('https://github.com/ZhonganTechENG/zarm')}>github</Menu.Item>
-          </Menu>
-        </nav> */}
       </header>
     );
   }
--- site/pages/Index/index.jsx
+++ site/pages/Index/index.jsx
@@ -1,5 +1,4 @@
 import React, { PureComponent } from 'react';
-import { Button } from 'dragon-ui';
 import Container from '@site/components/Container';
 import Header from '@site/components/Header';
 import './style.scss';
@@ -12,12 +11,24 @@ class Page extends PureComponent {
       <Container className="index-page">
         <Header />
         <main>
+          <div className="banner" ref={(ele) => { this.banner = ele; }}>
+            <div className="woman" />
+            <div className="buoy" />
+            <div className="buoy small" />
+          </div>
           <div className="introduce">
             <div className="title"><span>ZARM</span> DESIGN</div>
             <div className="description">一套为开发者、设计师和产品经理准备的组件库帮助你的网站快速成形</div>
-            <Button onClick={() => history.push('/documents/quick-start')}>开始使用</Button>
+            <div className="navigation">
+              <button onClick={() => history.push('/documents/quick-start')}>开始使用</button>
+              <button className="ghost" onClick={() => window.open('https://github.com/ZhonganTechENG/zarm')}>
+                <svg aria-hidden="true" version="1.1" viewBox="0 0 16 16">
+                  <path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                </svg>
+                <span>Github</span>
+              </button>
+            </div>
           </div>
-          <div className="banner" />
         </main>
       </Container>
     );
