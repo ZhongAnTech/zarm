799e1a874f02507635070e633fc18877952f42a5
yangpan <yangpan@yangpandeMacBook-Pro.local>
2018-11-14 18:17:01 +0800

    test: update test

--- components/marquee/__tests__/index.test.jsx
+++ components/marquee/__tests__/index.test.jsx
@@ -10,22 +10,26 @@ const createMarquee = (props) => {
   );
 };
 
+const trim = (str) => {
+  return str.replace(/[\r\n\s]/g, '');
+};
+
 describe('Marquee', () => {
   it('renders correctly', () => {
     const wrapper = render(
       <div>
-        { createMarquee({id: 1, height: 150, direction: 'right', width: '100%', loop: false, scrollAmount: 6, scrollDelay: 5, prefix: 'zarm-marquee'}) }
+        { createMarquee({ height: 150, direction: 'right', width: '100%', loop: false, scrollAmount: 6, scrollDelay: 5, prefix: 'zarm-marquee' }) }
       </div>
     );
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
   it('component props', () => {
-    const upMarquee = mount(createMarquee({id: 1, direction: 'up'}));
-    const leftMarquee = mount(createMarquee({id: 1, direction: 'left'}));
-    const rightMarquee = mount(createMarquee({id: 2, direction: 'right'}));
-    const downMarquee = mount(createMarquee({id: 3, direction: 'down'}));
-    const marquee = mount(createMarquee({id: 4, direction: ''}));
+    const upMarquee = mount(createMarquee({ direction: 'up' }));
+    const leftMarquee = mount(createMarquee({ direction: 'left' }));
+    const rightMarquee = mount(createMarquee({ direction: 'right' }));
+    const downMarquee = mount(createMarquee({ direction: 'down' }));
+    const marquee = mount(createMarquee({ direction: '' }));
     expect(leftMarquee.props().direction).toBe('left');
     expect(upMarquee.props().direction).toBe('up');
     expect(rightMarquee.props().direction).toBe('right');
@@ -35,36 +39,45 @@ describe('Marquee', () => {
 
   it('getKeyFrameModifierLeft', () => {
     const fn = getKeyFrameModifier('left');
-    const modifier = fn('50', 'zarm-marquee-1').replace(/[\r\n\s]/g,"");
-    expect(modifier).toEqual(`@-webkit-keyframeszarm-marquee-1{100%{-webkit-transform:translate3d(-50px,0,0);transform:translate3d(-50px,0,0);}}@keyframeszarm-marquee-1{100%{-webkit-transform:translate3d(-50px,0,0);transform:translate3d(-50px,0,0);}}`);
+    const modifier = trim(fn('50', 'zarm-marquee-1'));
+    const result = `@-webkit-keyframeszarm-marquee-1{100%{-webkit-transform:translate3d(-50px,0,0);transform:translate3d(-50px,0,0);}}
+    @keyframeszarm-marquee-1{100%{-webkit-transform:translate3d(-50px,0,0);transform:translate3d(-50px,0,0);}}`;
+    expect(modifier).toEqual(trim(result));
   });
 
   it('getKeyFrameModifierRight', () => {
     const fn = getKeyFrameModifier('up');
-    const modifier = fn('150', 'zarm-marquee-2').replace(/[\r\n\s]/g,"");
-    expect(modifier).toEqual(`@-webkit-keyframeszarm-marquee-2{100%{-webkit-transform:translate3d(0,-150px,0);transform:translate3d(0,-150px,0);}}@keyframeszarm-marquee-2{100%{-webkit-transform:translate3d(0,-150px,0);transform:translate3d(0,-150px,0);}}`);
+    const modifier = trim(fn('5', 'zarm-marquee-2'));
+    const result = `@-webkit-keyframeszarm-marquee-2{100%{-webkit-transform:translate3d(0,-5px,0);transform:translate3d(0,-5px,0);}}
+    @keyframeszarm-marquee-2{100%{-webkit-transform:translate3d(0,-5px,0);transform:translate3d(0,-5px,0);}}`;
+    expect(modifier).toEqual(trim(result));
   });
 
   it('getKeyFrameModifierRight', () => {
     const fn = getKeyFrameModifier('right');
-    const modifier = fn('100', 'zarm-marquee-3').replace(/[\r\n\s]/g,"");
-    expect(modifier).toEqual(`@-webkit-keyframeszarm-marquee-3{100%{-webkit-transform:translate3d(100px,0,0);transform:translate3d(100px,0,0);}}@keyframeszarm-marquee-3{100%{-webkit-transform:translate3d(100px,0,0);transform:translate3d(100px,0,0);}}`);
+    const modifier = trim(fn('100', 'zarm-marquee-3'));
+    const result = `@-webkit-keyframeszarm-marquee-3{100%{-webkit-transform:translate3d(100px,0,0);transform:translate3d(100px,0,0);}}
+    @keyframeszarm-marquee-3{100%{-webkit-transform:translate3d(100px,0,0);transform:translate3d(100px,0,0);}}`;
+    expect(modifier).toEqual(trim(result));
   });
 
   it('getKeyFrameModifierDown', () => {
     const fn = getKeyFrameModifier('down');
-    const modifier = fn('200', 'zarm-marquee-4').replace(/[\r\n\s]/g,"");
-    expect(modifier).toEqual(`@-webkit-keyframeszarm-marquee-4{100%{-webkit-transform:translate3d(0,200px,0);transform:translate3d(0,200px,0);}}@keyframeszarm-marquee-4{100%{-webkit-transform:translate3d(0,200px,0);transform:translate3d(0,200px,0);}}`);
+    const modifier = trim(fn('200', 'zarm-marquee-4'));
+    const result = `@-webkit-keyframeszarm-marquee-4{100%{-webkit-transform:translate3d(0,200px,0);transform:translate3d(0,200px,0);}}
+    @keyframeszarm-marquee-4{100%{-webkit-transform:translate3d(0,200px,0);transform:translate3d(0,200px,0);}}`;
+    expect(modifier).toEqual(trim(result));
   });
 
   it('getKeyFrameModifierDefault', () => {
     const fn = getKeyFrameModifier('default');
-    const modifier = fn('250', 'zarm-marquee-5').replace(/[\r\n\s]/g,"");
-    expect(modifier).toEqual(`@-webkit-keyframeszarm-marquee-5{100%{-webkit-transform:translate3d(-250px,0,0);transform:translate3d(-250px,0,0);}}@keyframeszarm-marquee-5{100%{-webkit-transform:translate3d(-250px,0,0);transform:translate3d(-250px,0,0);}}`);
+    const modifier = trim(fn('250', 'zarm-marquee-5'));
+    const result = `@-webkit-keyframeszarm-marquee-5{100%{-webkit-transform:translate3d(-250px,0,0);transform:translate3d(-250px,0,0);}}
+    @keyframeszarm-marquee-5{100%{-webkit-transform:translate3d(-250px,0,0);transform:translate3d(-250px,0,0);}}`;
+    expect(modifier).toEqual(trim(result));
   });
   it('animationModifier', () => {
     const modifier = animationModifier(5, true, 0, 'zarm-marquee-4');
-    expect(modifier).toEqual(`5s zarm-marquee-4 0s linear infinite`);
+    expect(modifier).toEqual('5s zarm-marquee-4 0s linear infinite');
   });
 });
-
