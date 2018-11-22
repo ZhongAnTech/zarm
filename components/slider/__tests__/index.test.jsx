342e69c02f7383088e67ca446604ef368953a003
develop-zw <wb_zhangwen@zhongan.io>
2018-11-22 10:18:08 +0800

    fix: updete slider test

--- components/slider/__tests__/index.test.jsx
+++ components/slider/__tests__/index.test.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
-import { render, mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Slider from '../index';
 
 describe('Slider', () => {
-  it('renders correctly', () => {
-    const wrapper = render(<Slider />);
+  it('shallows correctly', () => {
+    const wrapper = shallow(<Slider />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
   it('min', () => {
-    const wrapper = render(<Slider min={0} />);
+    const wrapper = shallow(<Slider min={0} />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
   it('max', () => {
-    const wrapper = render(<Slider max={100} />);
+    const wrapper = shallow(<Slider max={100} />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
   it('step', () => {
-    const wrapper = render(<Slider step={5} />);
+    const wrapper = shallow(<Slider step={5} />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
   it('disabled', () => {
-    const wrapper = render(<Slider disabled />);
+    const wrapper = shallow(<Slider disabled />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
   it('defaultValue', () => {
-    const wrapper = render(<Slider defaultValue={10} />);
+    const wrapper = shallow(<Slider defaultValue={10} />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
   it('value', () => {
-    const wrapper = render(<Slider value={10} />);
+    const wrapper = shallow(<Slider value={10} />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
