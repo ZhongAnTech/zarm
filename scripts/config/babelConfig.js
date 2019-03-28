b7a7da83a2044051e5057920135a78705bbb0419
Jerome Lin <jerome.lin@zhongan.com>
2019-03-28 10:55:13 +0800

    chore: add plugin-transform-runtime and corejs

--- scripts/config/babelConfig.js
+++ scripts/config/babelConfig.js
@@ -8,11 +8,10 @@ module.exports = {
   plugins: [
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-class-properties',
+    ['@babel/plugin-transform-runtime', { corejs: 2 }],
 
     // plugin-babel-import config
     ['import', { libraryName: 'zarm', libraryDirectory: 'components', style: true }, 'zarm'],
     ['import', { libraryName: 'dragon-ui', style: true }, 'dragon-ui'],
-
-    // '@babel/plugin-transform-runtime',
   ],
 };
