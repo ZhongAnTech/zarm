ceb12e5374461f0b8ce3b22f44f7ccd790e789d4
Jerome Lin <jerome.lin@zhongan.com>
2019-04-02 17:29:21 +0800

    fix: badge dot border-radius

--- components/badge/style/index.native.tsx
+++ components/badge/style/index.native.tsx
@@ -8,7 +8,7 @@ export default {
   dotBadge: {
     width: variables.badge_dot_diameter,
     height: variables.badge_dot_diameter,
-    borderRadius: variables.badge_round_radius,
+    borderRadius: variables.badge_dot_diameter,
   },
 
   radiusBadge: {
