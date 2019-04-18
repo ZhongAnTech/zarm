59f5dcefd622109f8c7482b04dbdceb352460a22
Jerome Lin <jerome.lin@zhongan.com>
2019-04-18 14:09:47 +0800

    chore: babel cli add --copy-files

--- scripts/gulp/gulpfile.js
+++ scripts/gulp/gulpfile.js
@@ -3,24 +3,17 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 const DIR = {
-  sass: path.resolve(__dirname, '../../components/**/*.scss'),
-  sassEntry: path.resolve(__dirname, '../../components/**/index.scss'),
+  sass: path.resolve(__dirname, '../../components/**/index.scss'),
   lib: path.resolve(__dirname, '../../lib'),
   es: path.resolve(__dirname, '../../es'),
 };
 
-gulp.task('copy', () => {
-  return gulp.src(DIR.sass)
-    .pipe(gulp.dest(DIR.lib))
-    .pipe(gulp.dest(DIR.es));
-});
-
 sass.compiler = require('sass');
 gulp.task('sass', () => {
-  return gulp.src(DIR.sassEntry)
+  return gulp.src(DIR.sass)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(DIR.lib))
     .pipe(gulp.dest(DIR.es));
 });
 
-gulp.task('default', gulp.series('copy', 'sass'));
+gulp.task('default', gulp.series('sass'));
