4f750d08f001796dcdda4b944d98ab56d1698754
Jerome Lin <jerome.lin@zhongan.com>
2019-04-18 10:56:48 +0800

    feat: support import css when use import plugin

--- scripts/gulp/gulpfile.js
+++ scripts/gulp/gulpfile.js
@@ -1,16 +1,26 @@
 const path = require('path');
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 const DIR = {
   sass: path.resolve(__dirname, '../../components/**/*.scss'),
+  sassEntry: path.resolve(__dirname, '../../components/**/index.scss'),
   lib: path.resolve(__dirname, '../../lib'),
   es: path.resolve(__dirname, '../../es'),
 };
 
-gulp.task('copySass', () => {
+gulp.task('copy', () => {
   return gulp.src(DIR.sass)
     .pipe(gulp.dest(DIR.lib))
     .pipe(gulp.dest(DIR.es));
 });
 
-gulp.task('default', gulp.series('copySass'));
+sass.compiler = require('sass');
+gulp.task('sass', () => {
+  return gulp.src(DIR.sassEntry)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(DIR.lib))
+    .pipe(gulp.dest(DIR.es));
+});
+
+gulp.task('default', gulp.series('copy', 'sass'));
