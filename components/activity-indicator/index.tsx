0e69adc562169db08d0f8ebb6158c02b1b89309a
Jerome Lin <jerome.lin@zhongan.com>
2019-05-31 14:03:52 +0800

    style: update code style

--- components/activity-indicator/index.tsx
+++ components/activity-indicator/index.tsx
@@ -23,11 +23,13 @@ const Circular = (props: ActivityIndicatorProps) => {
     strokeDasharray: `${(round * (percent as number)) / 100} ${round}`,
     strokeWidth,
   };
+
   if (loading) {
     const circularCls = classnames({
       [`${prefixCls}__circular`]: !size,
       [`${prefixCls}__circular--${size}`]: !!size,
     });
+
     return (
       <span className={cls}>
         <svg viewBox={`${DIAMETER / 2} ${DIAMETER / 2} ${DIAMETER} ${DIAMETER}`} className={circularCls}>
@@ -36,6 +38,7 @@ const Circular = (props: ActivityIndicatorProps) => {
       </span>
     );
   }
+
   return (
     <svg className={cls} viewBox={`0 0 ${DIAMETER} ${DIAMETER}`}>
       <circle className={`${prefixCls}__path`} cx={half} cy={half} r={r} fill="none" style={{ strokeWidth }} />
@@ -46,14 +49,15 @@ const Circular = (props: ActivityIndicatorProps) => {
 
 const Spinner = (props: ActivityIndicatorProps) => {
   const { prefixCls, className, size } = props;
-  const cls = classnames(prefixCls, className, {
+  const cls = classnames(prefixCls, `${prefixCls}__spinner`, className, {
     [`${prefixCls}--${size}`]: !!size,
-    [`${prefixCls}__spinner`]: true,
   });
   const spinner: any[] = [];
+
   for (let i = 0; i < 12; i++) {
     spinner.push(<div key={i} />);
   }
+
   return (
     <div className={cls}>{spinner}</div>
   );
