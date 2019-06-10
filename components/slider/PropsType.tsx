9b993dfa3d72f72d2130f82977353dd86d4d477a
wang <416834871@qq.com>
2019-06-10 18:39:22 +0800

    feat: add interface showMark & marks &  vertical

--- components/slider/PropsType.tsx
+++ components/slider/PropsType.tsx
@@ -1,3 +1,9 @@
+import { ReactNode } from 'react';
+
+export interface SliderMarks {
+  [key: number]: ReactNode;
+}
+
 export default interface PropsType {
   value?: number;
   defaultValue?: number;
@@ -5,5 +11,8 @@ export default interface PropsType {
   min: number;
   max: number;
   disabled?: boolean;
+  showMark?: boolean;
+  vertical?: boolean;
+  marks?: SliderMarks;
   onChange?: (value: number) => void;
 }
