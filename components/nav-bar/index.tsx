174a99733090a8a04045b8e5da704eb93d712cef
wang <416834871@qq.com>
2019-07-02 14:27:53 +0800

    fix: typo

--- components/nav-bar/index.tsx
+++ components/nav-bar/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import classnames from 'classnames';
-import BaseNavbarProps from './PropsType';
+import BaseNavBarProps from './PropsType';
 
-export interface NavBarProps extends BaseNavbarProps {
+export interface NavBarProps extends BaseNavBarProps {
   prefixCls?: string;
   className?: string;
 }
