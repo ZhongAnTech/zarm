2e60c18b319e38eac1cbfb5aa706b1da9ed35a03
yangpan <yangpan@yangpandeMacBook-Pro.local>
2019-08-21 14:46:53 +0800

    fix: picker android < 6.0, ios < 10 not work

--- components/picker/Picker.tsx
+++ components/picker/Picker.tsx
@@ -99,7 +99,6 @@ export default class Picker extends PureComponent<PickerProps, PickerState> {
     return (
       <Popup
         visible={visible}
-        mask={false}
         onMaskClick={maskClosable ? this.onCancel : noop}
       >
         <div className={cls} onClick={(e) => { e.stopPropagation(); }}>
