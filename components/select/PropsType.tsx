439ba5c34281aa8be2b08c46636bd78848ba5809
Jerome Lin <jerome.lin@zhongan.com>
2019-09-19 17:39:28 +0800

    style: code style format

--- components/select/PropsType.tsx
+++ components/select/PropsType.tsx
@@ -2,6 +2,7 @@ import BasePickerViewProps from '../picker-view/PropsType';
 import { Locale } from '../locale-provider/PropsType';
 
 type getContainerFunc = () => HTMLElement;
+
 export default interface BaseSelectProps extends BasePickerViewProps {
   title?: string;
   okText?: string;
