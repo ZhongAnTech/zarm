bf798e018284f4b9dc66fd31ef205ec376c058ba
Jerome Lin <jerome.lin@zhongan.com>
2019-09-19 17:25:18 +0800

    style: remove eslint-disable

--- components/utils/throttle.tsx
+++ components/utils/throttle.tsx
@@ -5,11 +5,10 @@ const throttle = (func, delay) => {
   return (...args: any[]) => {
     const curTime = Date.now();
     const remaining = delay - (curTime - startTime);
-    const context = this;  // eslint-disable-line
 
     clearTimeout(timer);
     if (remaining <= 0) {
-      func.apply(context, args);
+      func.apply(this, args);
       startTime = Date.now();
     } else {
       timer = setTimeout(func, remaining);
