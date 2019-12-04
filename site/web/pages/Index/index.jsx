8e4f8c6743c5b75cdc3c7aab9645dda5070ab854
Jerome Lin <jerome.lin@zhongan.com>
2019-12-04 16:47:01 +0800

    chore: update site

--- site/web/pages/Index/index.jsx
+++ site/web/pages/Index/index.jsx
@@ -36,7 +36,7 @@ class Page extends PureComponent {
               <span>Zarm</span>
               &nbsp;Design
             </div>
-            <div className="description">基于 React / React Native / Vue 的跨平台移动端 UI 组件库，为用户体验而生</div>
+            <div className="description">追求极致的用户体验，做有温度的组件库</div>
             <div className="navigation">
               <button type="button" onClick={() => history.push('/components/quick-start')}>开始使用</button>
               <Dropdown
