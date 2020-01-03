9c8bbc1772a0e14097162eb1e5dbd3d5b4c4401d
rou <wanglongguo@zhongan.io>
2020-01-03 20:24:10 +0800

    feat: enable progress.size
     now, we can set size of progress with a valid css length value,
     or just enum of `lg`, `md`, `sm`.
    
    BREAKING CHANGE: props of progress.weight has changed.
    
      to migrate the code follow the example below:
    
      Before:
    
      <Progress weight="normal" />
      <Progress weight="thin" />
    
      After:
    
      <Progress strokeWidth={8} />
      <Progress strokeWidth={4} />
    
      progress's prop weight has changed to strokeWidth.

--- components/progress/PropsType.tsx
+++ components/progress/PropsType.tsx
@@ -1,15 +1,40 @@
 import { CSSProperties } from 'react';
 
-export type ProgressWeight = 'bold' | 'normal' | 'thin';
-
-export type ProgressSize = 'lg' | 'md' | 'sm';
+export type KnownSize = 'lg' | 'md' | 'sm';
+export type ProgressTheme = 'primary' | 'success' | 'warning' | 'danger';
+export type ProgressSize = KnownSize | number | string;
+export type ProgressShape = 'line' | 'circle' | 'semi-circle';
+export type ProgressStrokeShape = 'round' | 'rect';
+export type ProgressPercent = number;
+export type PercentFormatter = (percent: number) => string;
 
 export default interface PropsType {
-  theme?: 'default' | 'primary' | 'success' | 'warning' | 'danger';
-  percent?: number;
-  type?: 'line' | 'circle' | 'semi-circle';
-  shape?: 'round' | 'rect';
+  // 类名前缀
+  prefixCls: string;
+
+  // 进度条主题
+  theme?: ProgressTheme;
+
+  // 进度条形状
+  shape?: ProgressShape;
+
+  // 进度条大小
   size?: ProgressSize;
-  weight?: ProgressWeight;
+
+  // 进度百分比
+  percent?: ProgressPercent;
+
+  // 进度条线的形状
+  strokeShape?: ProgressStrokeShape;
+
+  // 进度条线宽
+  strokeWidth?: number;
+
+  // 进度信息
+  text?: PercentFormatter;
+
+  // 进度条自定义样式
   style?: CSSProperties;
+
+  className?: string;
 }
--- components/progress/index.tsx
+++ components/progress/index.tsx
@@ -1,134 +1,109 @@
-import React, { PureComponent, CSSProperties } from 'react';
+import React, { CSSProperties, PureComponent } from 'react';
 import classnames from 'classnames';
-import PropsType, { ProgressWeight, ProgressSize } from './PropsType';
-
-export interface ProgressProps extends PropsType {
-  prefixCls?: string;
-  className?: string;
-  style?: CSSProperties;
-}
+import ProgressProps, { KnownSize } from './PropsType';
 
 type WeightMap = {
-  [weight in ProgressWeight]: number;
-};
-
-type SizeToWeight = {
-  [size in ProgressSize]: ProgressWeight
+  [weight in KnownSize]: number;
 };
 
 export default class Progress extends PureComponent<ProgressProps, any> {
   static defaultProps: ProgressProps = {
-    theme: 'default',
+    prefixCls: 'za-progress',
+    theme: 'primary',
+    shape: 'line',
     size: 'md',
     percent: 0,
-    type: 'line',
-    shape: 'round',
-    prefixCls: 'za-progress',
+    strokeShape: 'round',
+    text: (percent) => `${percent}%`,
   };
 
   // 线条粗细表
-  static weights: WeightMap = {
-    bold: 10,
-    normal: 8,
-    thin: 4,
+  static strokeWeights: WeightMap = {
+    lg: 10,
+    md: 8,
+    sm: 4,
   };
-
-  // 尺寸到线条粗细的映射表
-  // 使用size是因为它的可描述范围更广
-  // 保留weight是因为需要兼容以前的API
-  static sizeToWeight: SizeToWeight = {
-    lg: 'bold',
-    md: 'normal',
-    sm: 'thin',
-  };
-
-  private static cssVariablesPicked = false;
-
-  // 线条的宽度，共有两处配置
-  // e.g.  --progress-weight-lg && Progress.weights.bold
-  // 策略就是，当有Progress在BOM中渲染了后，如果BOM中有window.getComputedStyle这个API，就会把配置从css往js里拷一遍
-  // 根据奥卡姆剃刀定律，这就是最佳策略
-  static syncVariablesFromStyles(): void {
-    if (Progress.cssVariablesPicked) return;
-    Object.keys(Progress.weights).forEach((size) => {
-      const weightValue = Progress.pickVariable(`--progress-weight-${size}`);
-      if (weightValue) {
-        const weightName = Progress.sizeToWeight[size];
-        Progress.weights[weightName] = weightValue;
-      }
-    });
-    Progress.cssVariablesPicked = true;
-  }
-
-  static pickVariable(name: string): string {
-    return window!.getComputedStyle(document!.querySelector(':root') as Element)!.getPropertyValue(name);
-  }
 
   private progressElement;
 
   constructor(props) {
     super(props);
     this.state = {
-      strokeWidth: this.strokeWeight,
+      svgStrokeWidth: this.strokeWidth,
     };
   }
 
   componentDidMount() {
-    const { type } = this.props;
-    if (type === 'circle' || type === 'semi-circle') {
-      this.resetStrokeWidth();
+    if (this.useSVG) {
+      this.resetSVGStrokeWidth();
     }
   }
 
   componentDidUpdate(prevProps) {
-    const { weight } = this.props;
-    if (prevProps.weight !== weight) {
-      // preferWeight change only weight changed.
-      this.resetStrokeWidth();
+    const { props } = this;
+    if (prevProps.strokeWidth !== props.strokeWidth) {
+      this.resetSVGStrokeWidth();
     }
   }
 
-  get preferWeight() {
-    const { weight, size } = this.props;
-    return weight || (Progress.sizeToWeight[size || 'md'] as ProgressWeight);
+  get useSVG() {
+    const { props } = this;
+    return props.shape === 'semi-circle' || props.shape === 'circle';
   }
 
-  // 线条宽度系数 (weight 不同 width)
-  get strokeWeight() {
-    return Progress.weights[this.preferWeight];
+  get strokeWidth() {
+    const { strokeWidth, size } = this.props;
+    const { strokeWeights } = Progress;
+    const backup = strokeWeights[(size && (size in strokeWeights)) ? size : 'md'];
+    return Math.max(1, strokeWidth || backup);
   }
 
-  resetStrokeWidth() {
+  resetSVGStrokeWidth() {
     const baseWidth = 32;
     const { clientWidth } = this.progressElement;
 
     this.setState({
-      strokeWidth: (baseWidth / clientWidth) * this.strokeWeight,
+      svgStrokeWidth: (baseWidth / clientWidth) * this.strokeWidth,
     });
   }
 
   render() {
-    const { theme, percent, shape, type, size, style, prefixCls, className, children } = this.props;
-    const weight = this.preferWeight;
-    const { strokeWidth } = this.state;
+    const { theme, percent, strokeShape/* 进度条线的形状 */, shape, size, style, prefixCls, className, children, text: format } = this.props;
+    const { state } = this;
+    const strokeWidth = this.useSVG ? state.svgStrokeWidth : this.strokeWidth;
+    const hasKnownSize = size && (size in Progress.strokeWeights);
 
     const cls = classnames(prefixCls, className, {
-      [`${prefixCls}--${type}`]: !!type,
+      [`${prefixCls}--${shape}`]: !!shape,
       [`${prefixCls}--${theme}`]: !!theme,
-      [`${prefixCls}--${size}`]: !!size,
-      [`${prefixCls}--${weight}`]: !!weight,
+      [`${prefixCls}--${size}`]: hasKnownSize,
     });
+
+    const sizeStyle: CSSProperties = {};
+    if (!hasKnownSize) {
+      sizeStyle.width = size;
+      if (shape === 'circle') {
+        sizeStyle.height = size;
+      }
+      if (shape === 'semi-circle') {
+        if (typeof size === 'number') {
+          sizeStyle.height = `${size / 2}px`;
+        } else if (typeof size === 'string') {
+          sizeStyle.height = size.replace(/^(\d+)(.+)$/, (...$) => parseFloat($[1]) / 2 + $[2]);
+        }
+      }
+    }
 
     const diameter = 32;
     const radius = diameter / 2;
     const extendRadius = radius + strokeWidth / 2;
-    const strokeLinecap = shape === 'round' ? 'round' : 'butt';
+    const strokeLinecap = strokeShape === 'round' ? 'round' : 'butt';
 
-    const viewBox = type === 'circle'
+    const viewBox = shape === 'circle'
       ? `0 0 ${diameter + strokeWidth} ${diameter + strokeWidth}`
       : `0 0 ${diameter + strokeWidth} ${(diameter + strokeWidth) / 2}`;
 
-    const path = type === 'circle'
+    const path = shape === 'circle'
       ? `
         M${extendRadius}, ${extendRadius}
         m0 ${-radius}
@@ -139,11 +114,11 @@ export default class Progress extends PureComponent<ProgressProps, any> {
         m${-radius} 0
         a${radius} ${radius} 0 0 1 ${diameter} 0`;
 
-    const dasharray = type === 'circle'
+    const dasharray = shape === 'circle'
       ? Math.PI * diameter
       : (Math.PI * diameter) / 2;
 
-    const roundInner = (type === 'circle' || type === 'semi-circle')
+    const roundInner = (shape === 'circle' || shape === 'semi-circle')
       && (
         <svg viewBox={viewBox}>
           <path
@@ -163,26 +138,30 @@ export default class Progress extends PureComponent<ProgressProps, any> {
         </svg>
       );
 
-    const borderRadius = shape === 'round' ? `${this.strokeWeight}px` : '0';
-    const lineThumbStyle = { width: `${percent}%`, borderRadius };
+    const borderRadius = strokeShape === 'round' ? `${this.strokeWidth}px` : '0';
+    const lineTrackStyle = { height: `${strokeWidth}`, borderRadius };
+    const lineThumbStyle = { width: `${percent}%`, height: `${strokeWidth}px`, borderRadius };
 
     const rectInner = (
-      type === 'line'
+      shape === 'line'
       && (
-        <div className={`${prefixCls}__track`}>
+        <div className={`${prefixCls}__track`} style={lineTrackStyle}>
           <div className={`${prefixCls}__thumb`} style={lineThumbStyle} />
         </div>
       )
     );
 
+    const formattedPercent = format ? format(percent || 0) : null;
+    const hasIndicator = children || formattedPercent;
+
     return (
       <div
         className={cls}
-        style={style}
+        style={{ ...sizeStyle, ...style }}
         ref={(ele) => { this.progressElement = ele; }}
       >
-        {type === 'line' ? rectInner : roundInner}
-        {children && <div className={`${prefixCls}__text`}>{children}</div>}
+        {shape === 'line' ? rectInner : roundInner}
+        {hasIndicator && <div className={`${prefixCls}__text`}>{children || formattedPercent}</div>}
       </div>
     );
   }
