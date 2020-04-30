3a2d97c832d73a2d24a0d589fa04105adc4dde50
Jerome Lin <jerome.lin@zhongan.com>
2020-04-30 15:31:20 +0800

    chore: upgrade react-router-dom to 5.1.2

--- site/demo/components/App/index.jsx
+++ site/demo/components/App/index.jsx
@@ -1,5 +1,5 @@
-import React, { Component, Suspense, lazy } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
 import Loadable from 'react-loadable';
 import { Loading } from 'zarm';
@@ -22,7 +22,7 @@ const LoadableComponent = (component) => {
     render: (loaded, props) => {
       return (
         <Markdown
-          document={loaded.page.default}
+          content={loaded.page.default}
           component={component}
           {...props}
         />
@@ -32,27 +32,24 @@ const LoadableComponent = (component) => {
   });
 };
 
-class App extends Component {
-  render() {
-    const { location } = this.props;
-    const { general, form, feedback, view, navigation, other } = components;
+const App = () => {
+  const { general, form, feedback, view, navigation, other } = components;
+  return (
+    <SentryBoundary>
+      <Suspense fallback={<Loading visible />}>
+        <Switch>
+          <Route exact path="/" component={lazy(() => import('@site/demo/pages/Index'))} />
+          {
+            [...general, ...form, ...feedback, ...view, ...navigation, ...other].map((component, i) => (
+              <Route key={+i} path={`/${component.key}`} component={LoadableComponent(component)} />
+            ))
+          }
+          <Route component={lazy(() => import('@site/demo/pages/NotFoundPage'))} />
+        </Switch>
+        <div />
+      </Suspense>
+    </SentryBoundary>
+  );
+};
 
-    return (
-      <SentryBoundary>
-        <Suspense fallback={<Loading visible />}>
-          <Switch key={location.pathname} location={location}>
-            <Route exact path="/" component={lazy(() => import('@site/demo/pages/Index'))} />
-            {
-              [...general, ...form, ...feedback, ...view, ...navigation, ...other].map((component, i) => (
-                <Route key={+i} path={`/${component.key}`} component={LoadableComponent(component)} />
-              ))
-            }
-            <Route component={lazy(() => import('@site/demo/pages/NotFoundPage'))} />
-          </Switch>
-        </Suspense>
-      </SentryBoundary>
-    );
-  }
-}
-
-export default hot(withRouter(App));
+export default hot(App);
--- site/demo/components/Markdown.jsx
+++ site/demo/components/Markdown.jsx
@@ -1,38 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import marked from 'marked';
 // import { NavBar, Radio, Icon } from 'zarm';
+import { useLocation } from 'react-router-dom';
 import Demo from './Demo';
 import Container from './Container';
 import Footer from './Footer';
 
-export default class Markdown extends React.Component {
-  constructor(props) {
-    super(props);
-    // this.style = null;
-    this.components = new Map();
-    this.nodeList = [];
-  }
+export default (props) => {
+  const { content, component } = props;
+  const components = new Map();
+  const nodeList = [];
 
-  componentDidMount() {
-    this.renderDOM();
-  }
+  if (typeof content !== 'string') return null;
 
-  componentDidUpdate() {
-    this.renderDOM();
-  }
-
-  componentWillUnmount() {
-    this.nodeList.forEach((node) => {
-      ReactDOM.unmountComponentAtNode(node);
-    });
-  }
-
-  renderDOM() {
+  const renderDOM = () => {
     // eslint-disable-next-line
-    for (const [id, component] of this.components) {
+    for (const [id, component] of components) {
       const div = document.getElementById(id);
-      this.nodeList.push(div);
+      nodeList.push(div);
       if (div instanceof HTMLElement) {
         ReactDOM.render(component, div);
       }
@@ -44,59 +30,45 @@ export default class Markdown extends React.Component {
     // style.type = 'text/css';
     // style.appendChild(document.createTextNode(this.style));
     // head.appendChild(style);
-  }
+  };
 
-  render() {
-    const { document, component } = this.props;
-    if (typeof document === 'string') {
-      this.components.clear();
+  useEffect(() => {
+    renderDOM();
 
-      // document.replace(/<style>\s?([^]+?)(<\/style>)/g, (match, p1) => {
-      //   this.style = p1;
-      // });
+    return function cleanup() {
+      nodeList.forEach((node) => {
+        ReactDOM.unmountComponentAtNode(node);
+      });
+    };
+  });
 
-      const html = marked(
-        document
-          .replace(/## 自定义 Iconfont 图标\s?([^]+)/g, '') // 排除无法展示示例的情况
-          .replace(/## API\s?([^]+)/g, '') // 排除API显示
-          .replace(/##\s?([^]+?)((?=##)|$)/g, (match, p1) => {
-            const id = parseInt(Math.random() * 1e9, 10).toString(36);
-            this.components.set(id, React.createElement(Demo, this.props, p1));
-            return `<div id=${id}></div>`;
-          }),
-        {
-          renderer: new marked.Renderer(),
-        },
-      );
+  // doc.replace(/<style>\s?([^]+?)(<\/style>)/g, (match, p1) => {
+  //   style = p1;
+  // });
 
-      // const leftControl = (
-      //   <Icon
-      //     type="arrow-left"
-      //     theme="success"
-      //     onClick={() => window.history.back()}
-      //   />
-      // );
+  const html = marked(
+    content
+      .replace(/## 自定义 Iconfont 图标\s?([^]+)/g, '') // 排除无法展示示例的情况
+      .replace(/## API\s?([^]+)/g, '') // 排除API显示
+      .replace(/##\s?([^]+?)((?=##)|$)/g, (match, p1) => {
+        const id = parseInt(Math.random() * 1e9, 10).toString(36);
+        components.set(id, React.createElement(Demo, { ...props, location: useLocation() }, p1));
+        return `<div id=${id}></div>`;
+      }),
+    {
+      renderer: new marked.Renderer(),
+    },
+  );
 
-      // const rightControl = (
-      //   <Radio.Group type="button">
-      //     <Radio value="zh_CN">中文</Radio>
-      //     <Radio value="en_US">EN</Radio>
-      //   </Radio.Group>
-      // );
-
-      return (
-        <Container className={`${component.key}-page`}>
-          <main dangerouslySetInnerHTML={{ __html: html }} />
-          {/* <NavBar
-            style={{ position: 'fixed', top: 0 }}
-            title={`${data.name} ${data.description}`}
-            left={leftControl}
-          /> */}
-          <Footer />
-        </Container>
-      );
-    }
-
-    return <span />;
-  }
-}
+  return (
+    <Container className={`${component.key}-page`}>
+      <main dangerouslySetInnerHTML={{ __html: html }} />
+      {/* <NavBar
+    style={{ position: 'fixed', top: 0 }}
+    title={`${data.name} ${data.description}`}
+    left={leftControl}
+  /> */}
+      <Footer />
+    </Container>
+  );
+};
