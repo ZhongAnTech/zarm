338fa5d5f6199daeb38a164bc3c567a5faaa2d6c
Jerome Lin <jerome.lin@zhongan.com>
2020-06-02 18:39:09 +0800

    chore: update site

--- site/web/components/SideBar/index.jsx
+++ site/web/components/SideBar/index.jsx
@@ -1,69 +1,65 @@
-import React, { PureComponent } from 'react';
-import { withRouter } from 'react-router-dom';
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import { pascalCase } from 'change-case';
-import { Menu } from 'dragon-ui';
+import { Menu } from 'zarm-web';
 import { documents, components } from '@site/site.config';
 import './style.scss';
 
-class SideBar extends PureComponent {
-  getDocs = () => {
-    return documents.map((doc) => (
-      <Menu.Item
-        key={doc.key}
-      >
-        <a href={`#/components/${doc.key}`}>
-          {doc.name}
-        </a>
-      </Menu.Item>
-    ));
-  };
+const getDocs = () => {
+  return documents.map((doc) => (
+    <Menu.Item
+      key={doc.key}
+    >
+      <a href={`#/components/${doc.key}`}>{doc.name}</a>
+    </Menu.Item>
+  ));
+};
 
-  getMenus = (groupName, key) => {
-    const list = components[key] || [];
+const getMenus = (groupName, key) => {
+  const list = components[key] || [];
+  return (
+    <Menu.ItemGroup title={groupName} key={key}>
+      {
+        list
+          .sort((a, b) => {
+            return a.key.localeCompare(b.key);
+          })
+          .map((component) => (
+            <Menu.Item key={component.key}>
+              <a href={`#/components/${component.key}`}>
+                <span>{pascalCase(component.key)}</span>
+                <span className="chinese">{component.name}</span>
+              </a>
+            </Menu.Item>
+          ))
+      }
+    </Menu.ItemGroup>
+  );
+};
 
-    return (
-      <Menu.SubMenu title={groupName} key={key}>
-        {
-          list
-            .sort((a, b) => {
-              return a.key.localeCompare(b.key);
-            })
-            .map((component) => (
-              <Menu.Item key={component.key}>
-                <a href={`#/components/${component.key}`}>
-                  <span>{pascalCase(component.key)}</span>
-                  <span className="chinese">{component.name}</span>
-                </a>
-              </Menu.Item>
-            ))
-        }
-      </Menu.SubMenu>
-    );
-  };
+const SideBar = () => {
+  const params = useParams();
 
-  render() {
-    const { match } = this.props;
-    return (
-      <div className="side-bar">
-        <div className="menu">
-          <Menu
-            defaultOpenKeys={['components', 'general', 'form', 'feedback', 'view', 'navigation', 'other']}
-            selectedKeys={[match.params.document, match.params.component]}
-          >
-            {this.getDocs()}
-            <Menu.SubMenu title="组件" key="components">
-              {this.getMenus('通用', 'general')}
-              {this.getMenus('数据录入', 'form')}
-              {this.getMenus('数据展示', 'view')}
-              {this.getMenus('操作反馈', 'feedback')}
-              {this.getMenus('导航', 'navigation')}
-              {this.getMenus('其他', 'other')}
-            </Menu.SubMenu>
-          </Menu>
-        </div>
+  return (
+    <div className="side-bar">
+      <div className="menu">
+        <Menu
+          defaultOpenKeys={['components', 'general', 'form', 'feedback', 'view', 'navigation', 'other']}
+          selectedKeys={[params.document, params.component]}
+        >
+          {getDocs()}
+          <Menu.SubMenu title="组件" key="components">
+            {getMenus('通用', 'general')}
+            {getMenus('数据录入', 'form')}
+            {getMenus('数据展示', 'view')}
+            {getMenus('操作反馈', 'feedback')}
+            {getMenus('导航', 'navigation')}
+            {getMenus('其他', 'other')}
+          </Menu.SubMenu>
+        </Menu>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default withRouter(SideBar);
+export default SideBar;
