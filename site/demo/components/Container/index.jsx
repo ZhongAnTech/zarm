a684cf41027243095bd9d8fd50760a12a225d96d
Jerome Lin <jerome.lin@zhongan.com>
2020-06-05 15:39:47 +0800

    chore: support internationalization and custom theme

--- site/demo/components/Container/index.jsx
+++ site/demo/components/Container/index.jsx
@@ -1,49 +1,79 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState, createContext } from 'react';
 import classnames from 'classnames';
-import { Icon } from 'zarm';
+import { Icon, Radio, Popper } from 'zarm';
+import { darken } from '@site/utils/color';
 import './style.scss';
 
 const Icons = Icon.createFromIconfont('//at.alicdn.com/t/font_1340918_lpsswvb7yv.js');
 
-class Container extends Component {
-  // state = {
-  //   language: window.localStorage.language || 'zh_CN',
-  // };
+export const Context = createContext({});
 
-  componentDidMount() {
+const Container = (props) => {
+  const [lang, setLang] = useState(window.sessionStorage.language || 'zhCN');
+  const [theme, setTheme] = useState(window.sessionStorage.theme || '#00bc70');
+
+  const setCssVar = (color) => {
+    document.documentElement.style.setProperty('--theme-primary', color);
+    document.documentElement.style.setProperty('--theme-primary-dark', darken(color, 0.04));
+  };
+
+  useEffect(() => {
     window.scrollTo(0, 0);
-  }
+    setCssVar(theme);
+  }, [theme]);
 
-  render() {
-    const { className, children } = this.props;
-    const cls = classnames('app-container', className);
-    // const { language } = this.state;
+  const { className, children } = props;
+  const cls = classnames('app-container', className);
 
-    return (
-      <div className={cls}>
+  return (
+    <div className={cls}>
+      <nav>
+        <div className="lang">
+          <Radio.Group
+            compact
+            type="button"
+            value={lang}
+            onChange={(value) => {
+              setLang(value);
+              window.sessionStorage.language = value;
+            }}
+          >
+            <Radio value="zhCN">中文</Radio>
+            <Radio value="enUS">EN</Radio>
+          </Radio.Group>
+        </div>
+        <Popper
+          trigger="click"
+          content={(
+            <ul className="color-pick-list">
+              {
+                ['#00bc70', '#1890ff', '#f5222d', '#fa541b', '#13c2c2', '#2f54ec', '#712fd1'].map((color, index) => {
+                  return (
+                    <li
+                      key={+index}
+                      style={{ backgroundColor: color }}
+                      onClick={() => {
+                        setTheme(color);
+                        window.sessionStorage.theme = color;
+                      }}
+                    />
+                  );
+                })
+              }
+            </ul>
+        )}
+        >
+          <span className="color-pick" style={{ backgroundColor: theme }} />
+        </Popper>
         <a className="github" href="https://github.com/ZhongAnTech/zarm">
           <Icons type="github" size="lg" />
         </a>
-        {/* <div className="lang">
-          <Radio.Group
-            type="button"
-            value={language}
-            onChange={(value) => {
-              this.setState({
-                language: value,
-              });
-              window.localStorage.language = value;
-              window.location.reload();
-            }}
-          >
-            <Radio value="zh_CN">中文</Radio>
-            <Radio value="en_US">EN</Radio>
-          </Radio.Group>
-        </div> */}
+      </nav>
+      <Context.Provider value={{ lang, theme }}>
         {children}
-      </div>
-    );
-  }
-}
+      </Context.Provider>
+    </div>
+  );
+};
 
 export default Container;
