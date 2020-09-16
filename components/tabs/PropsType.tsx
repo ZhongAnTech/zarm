446da04a32a7819c56992daa17fd97da3bfee0cf
JeromeLin <jerome.lin@zhongan.com>
2020-09-16 18:45:36 +0800

    fix: remove `scrollThreshold` props of tabs. fix #497

--- components/tabs/PropsType.tsx
+++ components/tabs/PropsType.tsx
@@ -1,7 +1,6 @@
 export default interface PropsType {
   value?: number;
   defaultValue?: number;
-  scrollThreshold: number;
   lineWidth?: string | number;
   disabled?: boolean;
   swipeable?: boolean;
