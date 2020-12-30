7fd2181a64f41eef02d635847a795e08421648dd
吃土小2叉（Tony Xu） <xxdalston@gmail.com>
2020-12-30 18:13:45 +0800

    Update PropsType.tsx

--- components/date-picker-view/PropsType.tsx
+++ components/date-picker-view/PropsType.tsx
@@ -14,5 +14,5 @@ export default interface BaseDatePickerViewProps {
   valueMember?: string;
   onTransition?: (value: boolean) => void;
   locale?: Locale['DatePickerView'] & Locale['DatePicker'] & Locale['DateSelect'];
-  rollDuration: number;
+  rollDuration?: number;
 }
