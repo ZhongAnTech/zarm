615a17fa9763d99db67314ef48dde9647d14ebfd
吃土小2叉（Tony Xu） <xxdalston@gmail.com>
2020-12-30 17:17:38 +0800

    fix: scrollOptions 改为 rollDuration

--- components/wheel/Wheel.tsx
+++ components/wheel/Wheel.tsx
@@ -37,7 +37,7 @@ export default class Wheel extends Component<WheelProps, any> {
   };
 
   componentDidMount() {
-    const { prefixCls, dataSource, disabled, onTransition, valueMember, scrollOptions } = this.props;
+    const { prefixCls, dataSource, disabled, onTransition, valueMember, rollDuration } = this.props;
     const value = getValue(this.props);
     const initIndex = this.getSelectedIndex(value, dataSource);
     this.BScroll = new BScroll(this.wrapper, {
@@ -47,7 +47,7 @@ export default class Wheel extends Component<WheelProps, any> {
         wheelItemClass: `${prefixCls}-item`,
       },
       probeType: 3,
-      ...scrollOptions
+      swipeTime: rollDuration || 2500,
     });
 
     disabled && this.BScroll.disable();
