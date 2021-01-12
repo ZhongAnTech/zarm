c7507ca5db8bd2c829dbe429ec559dd153c53405
Jerome Lin <jerome.lin@zhongan.com>
2021-01-12 14:47:40 +0800

    perf: optimize button-primary-shadow-color

--- components/config-provider/setPrimaryColor.ts
+++ components/config-provider/setPrimaryColor.ts
@@ -3,7 +3,7 @@ import Color from 'color';
 const setPrimaryColor = (color) => {
   document.documentElement.style.setProperty('--theme-primary', color);
   document.documentElement.style.setProperty('--theme-primary-dark', Color(color).darken(0.05));
-  document.documentElement.style.setProperty('--button-primary-shadow-color', Color(color).alpha(0.5).lighten(0.0));
+  document.documentElement.style.setProperty('--button-primary-shadow-color', Color(color).alpha(0.3));
 };
 
 export default setPrimaryColor;
