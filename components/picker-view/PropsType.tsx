09651f4cbf3682c506d38bd77d43b20c40778cb5
edison-hm <490276219@qq.com>
2021-01-13 15:21:28 +0800

    refactor: update dataSource type

--- components/picker-view/PropsType.tsx
+++ components/picker-view/PropsType.tsx
@@ -1,6 +1,6 @@
 import BaseWheelProps, { WheelValue, WheelItem } from '../wheel/PropsType';
 
-export type DataSource = Array<{ [key: string]: WheelValue | DataSource }>;
+export type DataSource<T = { label: string; value: WheelValue }> = Array<T & { children?: DataSource<T> }>;
 
 export default interface BasePickerViewProps extends Pick<BaseWheelProps, 'valueMember' | 'itemRender' | 'disabled' | 'stopScroll'> {
   value?: WheelValue | Array<WheelValue>;
