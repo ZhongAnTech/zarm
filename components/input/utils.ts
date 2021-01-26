aa7034e5e04bbf77205667ceeec4f1430aa6c3a4
yangpan <yangpan@yangpandeMacBook-Pro.local>
2021-01-26 20:40:23 +0800

    fix: #551

--- components/input/utils.ts
+++ components/input/utils.ts
@@ -1,8 +1,8 @@
 export function getValue(props, defaultValue: string | number = ''): string | number {
-  if (typeof props.value !== 'undefined') {
+  if (typeof props.value !== 'undefined' && props.value !== null ) {
     return props.value;
   }
-  if (typeof props.defaultValue !== 'undefined') {
+  if (typeof props.defaultValue !== 'undefined' && props.defaultValue !== null) {
     return props.defaultValue;
   }
   return defaultValue;
