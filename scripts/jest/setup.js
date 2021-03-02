a5c305c6f95a69e813cea45c0581555768867620
Jerome Lin <jerome.lin@zhongan.com>
2021-03-02 19:59:13 +0800

    chore: add workflow for react 16 and 17

--- scripts/jest/setup.js
+++ scripts/jest/setup.js
@@ -1,5 +1,8 @@
 const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
+const Adapter =
+  process.env.REACT === '16'
+    ? require('enzyme-adapter-react-16')
+    : require('@wojtekmaj/enzyme-adapter-react-17');
 
 Enzyme.configure({ adapter: new Adapter() });
 
