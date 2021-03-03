9f9fbc95f1536f6d17d0846f909bcded7f25ee0f
mrdulin <novaline.dulin@gmail.com>
2021-03-03 10:09:13 +0800

    test: add unit tests for ConfigProvider

--- components/config-provider/__tests__/index.test.tsx
+++ components/config-provider/__tests__/index.test.tsx
@@ -1,19 +1,28 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import React from 'react';
-import { render } from 'enzyme';
+import { mount, render, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import LocaleProvider from '../index';
 import Alert from '../../alert';
 import localeCN from '../locale/zh_CN';
 import localeEN from '../locale/en_US';
 import setTheme, { themes } from '../setTheme';
+import setPrimaryColor from '../setPrimaryColor';
+import { ConfigContext } from '../ConfigProvider';
+
+class TestComponent extends React.Component {
+  static contextType = ConfigContext;
+
+  render() {
+    const { theme } = this.context;
+    return <div>{theme}</div>;
+  }
+}
 
 describe('setTheme', () => {
   afterEach(() => {
     jest.restoreAllMocks();
     document.body.removeAttribute('data-theme');
-    Object.keys(themes).forEach((key) => {
-      document.documentElement.style.removeProperty(key);
-    });
+    document.documentElement.style.cssText = '';
   });
   it('should set dark theme', () => {
     expect.assertions(4 + Object.keys(themes).length);
@@ -44,26 +53,126 @@ describe('setTheme', () => {
   });
 });
 
-describe('ConfigProvider', () => {
-  it('should pass', () => {});
+describe('setPrimaryColor', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+    document.documentElement.style.cssText = '';
+  });
+  it('should set primary theme color', () => {
+    const setPropertySpy = jest.spyOn(document.documentElement.style, 'setProperty');
+    setPrimaryColor('#00bc70');
+    expect(setPropertySpy).toBeCalledWith('--theme-primary', '#00bc70');
+    expect(setPropertySpy).toBeCalledWith(
+      '--theme-primary-dark',
+      'hsl(155.70000000000005, 100%, 35%)',
+    );
+    expect(setPropertySpy).toBeCalledWith(
+      '--theme-primary-lighter',
+      'hsl(155.70000000000005, 100%, 95%)',
+    );
+    expect(setPropertySpy).toBeCalledWith(
+      '--button-primary-shadow-color',
+      'rgba(0, 188, 112, 0.3)',
+    );
+    expect(document.documentElement.style.cssText).toMatchInlineSnapshot(
+      `"--theme-primary: #00bc70; --theme-primary-dark: hsl(155.70000000000005, 100%, 35%); --theme-primary-lighter: hsl(155.70000000000005, 100%, 95%); --button-primary-shadow-color: rgba(0, 188, 112, 0.3);"`,
+    );
+  });
 });
 
-describe('LocaleProvider', () => {
-  it('renders correctly with zh_CN', () => {
-    const wrapper = render(
-      <LocaleProvider locale={localeCN}>
-        <Alert />
-      </LocaleProvider>,
-    );
-    expect(toJson(wrapper)).toMatchSnapshot();
+describe('ConfigProvider', () => {
+  describe('snapshot', () => {
+    let ConfigProvider: typeof import('../index').default;
+    beforeEach(() => {
+      ConfigProvider = require('../index').default;
+    });
+    it('renders correctly with zh_CN', () => {
+      const wrapper = render(
+        <ConfigProvider locale={localeCN}>
+          <Alert />
+        </ConfigProvider>,
+      );
+      expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('renders correctly with en_US', () => {
+      const wrapper = render(
+        <ConfigProvider locale={localeEN}>
+          <Alert />
+        </ConfigProvider>,
+      );
+      expect(toJson(wrapper)).toMatchSnapshot();
+    });
   });
 
-  it('renders correctly with en_US', () => {
-    const wrapper = render(
-      <LocaleProvider locale={localeEN}>
-        <Alert />
-      </LocaleProvider>,
-    );
-    expect(toJson(wrapper)).toMatchSnapshot();
+  describe('behaviour', () => {
+    let mod: typeof import('../ConfigProvider');
+    let ConfigProvider: typeof mod.default;
+    const mSetTheme = jest.fn();
+    const mSetPrimaryColor = jest.fn();
+
+    beforeEach(() => {
+      jest.resetModules();
+      jest.doMock('../setTheme', () => {
+        return {
+          __esModule: true,
+          default: mSetTheme,
+        };
+      });
+      jest.doMock('../setPrimaryColor', () => {
+        return {
+          __esModule: true,
+          default: mSetPrimaryColor,
+        };
+      });
+      mod = require('../ConfigProvider');
+      ConfigProvider = mod.default;
+    });
+    it('should change runtime locale, set theme, set primary theme color with default value', () => {
+      const { getRunTimeLocale } = mod;
+      const wrapper = shallow(
+        <ConfigProvider>
+          <></>
+        </ConfigProvider>,
+      );
+      const actual = getRunTimeLocale();
+      expect(actual).toEqual({});
+      expect(mSetTheme).toBeCalledWith('light');
+      expect(mSetPrimaryColor).toBeCalledWith('#00bc70');
+      expect(wrapper.prop('value')).toEqual({
+        locale: {},
+        theme: 'light',
+        primaryColor: '#00bc70',
+      });
+    });
+
+    it('should accept only one child(a React element)', () => {
+      expect(() =>
+        shallow(
+          <ConfigProvider>
+            <div />
+            <div />
+          </ConfigProvider>,
+        ),
+      ).toThrowError('React.Children.only expected to receive a single React element child.');
+    });
+
+    it('should get context from ConfigContext provider', () => {
+      const wrapper = mount(
+        <ConfigProvider>
+          <TestComponent />
+        </ConfigProvider>,
+      );
+      expect(wrapper.find('div').text()).toEqual('light');
+    });
+
+    it('should get context from ConfigContext provider', () => {
+      const wrapper = mount(
+        <ConfigProvider>
+          <ConfigContext.Consumer>{(value) => <div>{value.theme}</div>}</ConfigContext.Consumer>
+        </ConfigProvider>,
+      );
+      expect(wrapper.find('div').text()).toEqual('light');
+    });
   });
 });
