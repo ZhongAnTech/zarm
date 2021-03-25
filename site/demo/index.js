08d627d2ff12aff35262bca185b77bef21b7676f
Jerome Lin <jerome.lin@zhongan.com>
2021-03-25 15:50:36 +0800

    chore: update index.js

--- site/demo/index.js
+++ site/demo/index.js
@@ -5,12 +5,9 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import App from './components/App';
 
-const Page = () => {
-  return (
-    <HashRouter>
-      <App />
-    </HashRouter>
-  );
-};
-
-ReactDOM.render(<Page />, document.getElementById('app'));
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById('app'),
+);
