72c44cbd4c96e4901369c439e801ab4c157f4be9
Jerome Lin <jerome.lin@zhongan.com>
2021-05-10 11:39:13 +0800

    chore: update script for svg export

--- packages/zarm-icons/scripts/exportSVG.js
+++ packages/zarm-icons/scripts/exportSVG.js
@@ -1,12 +1,9 @@
-const fs = require('fs-extra');
 const path = require('path');
 const shell = require('shelljs');
 
 const svgDir = path.join(__dirname, '../svg');
 const sketch = path.join(__dirname, '../assets/icons.sketch');
 const SKETCH_TOOL_DIR = '/Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool';
-
-fs.removeSync(svgDir);
 
 // extract svg from sketch
 // should install sketchtool first
