567ed3da82b6c9c4cd876f124baf8cdc8243226f
JeromeLin <jerome.lin@zhongan.com>
2021-06-16 14:37:24 +0800

    style: format button and tabs code style (#656)

--- packages/zarm/src/tabs/TabPanel.tsx
+++ packages/zarm/src/tabs/TabPanel.tsx
@@ -7,24 +7,21 @@ export interface TabPanelProps extends BaseTabPanelProps {
   className?: string;
 }
 
-const TabPanel = React.forwardRef<unknown, TabPanelProps>((props, ref) => {
-  const {
-    prefixCls = 'za-tabs',
-    className,
-    selected,
-    children
-  } = props;
-  const panelRef = (ref as any) || React.createRef<HTMLElement>();
-  const cls = classnames(`${prefixCls}__panel`, className, {
-    [`${prefixCls}__panel--active`]: selected,
-  });
+const TabPanel = React.forwardRef<unknown, TabPanelProps>(
+  ({ prefixCls = 'za-tabs', className, selected, children }: TabPanelProps, ref) => {
+    const panelRef = (ref as any) || React.createRef<HTMLElement>();
 
-  return (
-    <div className={cls} role="tabpanel" ref={panelRef}>
-      {children}
-    </div>
-  );
-});
+    const cls = classnames(`${prefixCls}__panel`, className, {
+      [`${prefixCls}__panel--active`]: selected,
+    });
+
+    return (
+      <div className={cls} role="tabpanel" ref={panelRef}>
+        {children}
+      </div>
+    );
+  },
+);
 
 TabPanel.displayName = 'TabPanel';
 
