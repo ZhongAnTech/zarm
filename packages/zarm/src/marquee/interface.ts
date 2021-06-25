eccb47c7b61a6bb305f83f1efbeccea62d99e960
Yang <yangpan@zhongan.com>
2021-06-25 19:06:03 +0800

    fix: marquee code style

---
+++ packages/zarm/src/marquee/interface.ts
@@ -0,0 +1,8 @@
+export interface BaseMarqueeProps {
+  direction: 'left' | 'right' | 'up' | 'down';
+  loop?: boolean;
+  speed?: number;
+  animationDelay?: number;
+  height?: number;
+  width?: number;
+}
