9256b670e3ec5aa51a8ae51d207ee085b1d1c3cf
stbui <stbui@stbui.com>
2021-10-19 19:22:31 +0800

    refactor(picker): 重写picker

--- packages/zarm/src/picker/Picker.tsx
+++ packages/zarm/src/picker/Picker.tsx
@@ -1,145 +1,110 @@
-import React, { Component } from 'react';
-import isEqual from 'lodash/isEqual';
+import React, { useEffect, useContext, useState } from 'react';
+import classnames from 'classnames';
+
+import { ConfigContext } from '../n-config-provider';
 import Popup from '../popup';
 import PickerView from '../picker-view';
 import parseProps from '../picker-view/utils/parseProps';
-import removeFnFromProps from '../picker-view/utils/removeFnFromProps';
-import type { BasePickerProps } from './PropsType';
-import type { PickerDataSource } from '../picker-view/PropsType';
-import type { WheelValue, WheelItem } from '../wheel/interface';
+import type { BasePickerProps } from './interface';
 
-export interface PickerProps extends BasePickerProps {
-  prefixCls?: string;
-  className?: string;
-}
+export type PickerProps = BasePickerProps & React.HTMLAttributes<HTMLDivElement>;
 
-interface PickerState {
-  value: Array<WheelValue>;
-  objValue: Array<WheelItem>;
-  dataSource: PickerDataSource;
-  tempObjValue?: Array<WheelItem>;
-  tempValue?: Array<WheelValue>;
-  stopScroll?: boolean;
-}
+const Picker: React.FC<PickerProps> = (props) => {
+  const {
+    className,
+    cancelText,
+    okText,
+    title,
+    maskClosable,
+    mountContainer,
+    destroy,
+    onOk,
+    onCancel,
+    visible,
+    valueMember,
+    onChange,
+    itemRender,
+    dataSource,
+  } = props;
+  const [state, setState] = useState(parseProps.getSource(props));
 
-export default class Picker extends Component<PickerProps, PickerState> {
-  static defaultProps = {
-    dataSource: [],
-    prefixCls: 'za-picker',
-    valueMember: 'value',
-    cols: Infinity,
-    maskClosable: true,
-    itemRender: (data) => data.label,
-    destroy: false,
+  const { prefixCls: globalPrefixCls, locale } = useContext(ConfigContext);
+  const prefixCls = `${globalPrefixCls}-picker`;
+  const cls = classnames(prefixCls, className);
+
+  const onClickCancel = () => {
+    const { value = [], objValue = [] } = state.value;
+
+    setState((preState) => ({
+      ...preState,
+      value,
+      objValue,
+    }));
+
+    if (typeof onCancel === 'function') {
+      onCancel();
+    }
   };
 
-  state: PickerState = { ...parseProps.getSource(this.props), stopScroll: false };
-
-  static getDerivedStateFromProps(props, state) {
-    if (
-      !isEqual(
-        removeFnFromProps(props, ['onOk', 'onCancel', 'onChange']),
-        removeFnFromProps(state.prevProps, ['onOk', 'onCancel', 'onChange']),
-      )
-    ) {
-      return {
-        prevProps: props,
-        ...parseProps.getSource(props),
-        tempValue: parseProps.getSource(props).value,
-        tempObjValue: parseProps.getSource(props).objValue,
-      };
+  const onClickOk = () => {
+    if (typeof onOk === 'function') {
+      onOk(state.objValue);
     }
+  };
 
-    return null;
-  }
-
-  onChange = (selected) => {
-    const { valueMember, onChange } = this.props;
+  const onPickViewChange = (selected) => {
     const value = selected.map((item) => item[valueMember!]);
-    this.setState({ value, objValue: selected });
+    setState((prevState) => ({ ...prevState, value, objValue: selected }));
 
     if (typeof onChange === 'function') {
       onChange(selected);
     }
   };
 
-  onCancel = () => {
-    const { onCancel } = this.props;
-    const { tempValue = [], tempObjValue = [] } = this.state;
-    this.setState({
-      value: tempValue,
-      objValue: tempObjValue,
-    });
-    if (typeof onCancel === 'function') {
-      onCancel();
-    }
-  };
+  useEffect(() => {
+    setState(parseProps.getSource(props));
+  }, [dataSource]);
 
-  onOk = () => {
-    const { onOk } = this.props;
-    this.setState(
-      {
-        stopScroll: true,
-      },
-      () => {
-        this.setState(
-          {
-            stopScroll: false,
-          },
-          () => {
-            if (typeof onOk === 'function') {
-              onOk(this.state.objValue);
-            }
-          },
-        );
-      },
-    );
-  };
-
-  render() {
-    const {
-      prefixCls,
-      className,
-      cancelText,
-      okText,
-      title,
-      locale,
-      maskClosable,
-      mountContainer,
-      destroy,
-      onOk,
-      onCancel,
-      visible,
-      ...others
-    } = this.props;
-    const { value, stopScroll = false } = this.state;
-    const noop = () => {};
-    return (
-      <Popup
-        className={className}
-        visible={visible}
-        onMaskClick={maskClosable ? this.onCancel : noop}
-        mountContainer={mountContainer}
-        destroy={destroy}
+  return (
+    <Popup
+      visible={visible}
+      onMaskClick={maskClosable ? onClickCancel : () => {}}
+      mountContainer={mountContainer}
+      destroy={destroy}
+    >
+      <div
+        className={cls}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
       >
-        <div
-          className={prefixCls}
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
-        >
-          <div className={`${prefixCls}__header`}>
-            <div className={`${prefixCls}__cancel`} onClick={this.onCancel}>
-              {cancelText || locale!.cancelText}
-            </div>
-            <div className={`${prefixCls}__title`}>{title || locale!.title}</div>
-            <div className={`${prefixCls}__submit`} onClick={this.onOk}>
-              {okText || locale!.okText}
-            </div>
+        <div className={`${prefixCls}__header`}>
+          <div className={`${prefixCls}__cancel`} onClick={onClickCancel}>
+            {cancelText || locale?.Picker?.cancelText}
           </div>
-          <PickerView {...others} value={value} onChange={this.onChange} stopScroll={stopScroll} />
+          <div className={`${prefixCls}__title`}>{title || locale?.Picker?.title}</div>
+          <div className={`${prefixCls}__submit`} onClick={onClickOk}>
+            {okText || locale?.Picker?.okText}
+          </div>
         </div>
-      </Popup>
-    );
-  }
-}
+        <PickerView
+          dataSource={state.dataSource}
+          value={state.value}
+          itemRender={itemRender}
+          onChange={onPickViewChange}
+        />
+      </div>
+    </Popup>
+  );
+};
+
+Picker.defaultProps = {
+  dataSource: [],
+  valueMember: 'value',
+  cols: Infinity,
+  maskClosable: true,
+  itemRender: (data) => data.label,
+  destroy: false,
+};
+
+export default Picker;
