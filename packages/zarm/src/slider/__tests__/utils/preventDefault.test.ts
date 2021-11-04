735e6c3e98e3b9a2ca366d0cdb8b156c65e7230a
question <yangpan103@gmail.com>
2021-11-04 13:45:43 +0800

    refactor: refactor slider (#820)
    * refactor: slider
    
    * refactor: slider
    
    * refactor: slider
    
    * refactor: slider
    
    * refactor: slider
    
    * refactor: slider
    
    * refactor: slider update snap
    
    * refactor: slider update snap
    
    * refactor: slider update snap
    
    * refactor: slider dark theme
    
    Co-authored-by: Yang <yangpan@zhongan.com>

--- packages/zarm/src/slider/__tests__/utils/preventDefault.test.ts
+++
@@ -1,9 +0,0 @@
-import preventDefault from '../../utils/preventDefault';
-
-describe('preventDefault', () => {
-  it('should pass', () => {
-    const mEvent = { preventDefault: jest.fn() };
-    preventDefault((mEvent as unknown) as MouseEvent);
-    expect(mEvent.preventDefault).toBeCalledTimes(1);
-  });
-});
