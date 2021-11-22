4999800d47b1a319f4c1bd48fcabf7dca52887d9
Nick Na <naguangqing@qq.com>
2021-11-22 11:28:33 +0800

    refactor:  Marquee (#828)
    Co-authored-by: 那光情 <naguangqing@zhongan.com>

--- packages/zarm/src/__tests__/index.test.ts
+++ packages/zarm/src/__tests__/index.test.ts
@@ -212,7 +212,16 @@ describe('index', () => {
           "render": [Function],
           "useLoading": [Function],
         },
-        "Marquee": [Function],
+        "Marquee": Object {
+          "$$typeof": Symbol(react.forward_ref),
+          "defaultProps": Object {
+            "delay": 0,
+            "direction": "left",
+            "loop": true,
+            "speed": 30,
+          },
+          "render": [Function],
+        },
         "Mask": Object {
           "$$typeof": Symbol(react.forward_ref),
           "defaultProps": Object {
