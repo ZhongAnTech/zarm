936459e7e716063ed5f4a714a9eb5d6f36f564eb
JeromeLin <jerome.lin@zhongan.com>
2021-11-23 14:13:03 +0800

    perf: optimize HTMLAttributes for extends

--- packages/zarm/src/toast/Toast.tsx
+++ packages/zarm/src/toast/Toast.tsx
@@ -1,8 +1,9 @@
 import React, { RefAttributes, useCallback, useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
-import BaseToastProps, { HTMLDefProps } from './interface';
-import Popup from '../popup';
 import { ConfigContext } from '../n-config-provider';
+import Popup from '../popup';
+import type { BaseToastProps } from './interface';
+import type { HTMLDefProps } from '../utils/utilityTypes';
 
 export interface ToastProps extends BaseToastProps, HTMLDefProps {}
 export interface UseToast {
