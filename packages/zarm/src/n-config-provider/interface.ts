10517740b320daf9e47ea9e1baaddf6a55d75f75
JeromeLin <jerome.lin@zhongan.com>
2021-12-27 18:18:40 +0800

    fix: export type of locale

--- packages/zarm/src/n-config-provider/interface.ts
+++ packages/zarm/src/n-config-provider/interface.ts
@@ -2,9 +2,11 @@ import localeCN from './locale/zh_CN';
 import type { ContainerType } from '../utils/dom';
 import type { CssVars } from './cssVars';
 
+export type Locale = typeof localeCN;
+
 export interface ConfigProviderProps {
   prefixCls?: string;
-  locale?: typeof localeCN;
+  locale?: Locale;
   primaryColor?: string;
   theme?: 'dark' | 'light';
   safeIphoneX?: boolean;
