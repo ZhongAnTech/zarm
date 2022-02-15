bcf33fff245f42e2cc197424722722ce19a2476b
JeromeLin <jerome.lin@zhongan.com>
2022-02-15 17:28:01 +0800

    chore: add css vars define

--- packages/zarm/src/n-config-provider/cssVars.ts
+++ packages/zarm/src/n-config-provider/cssVars.ts
@@ -1,5 +1,11 @@
-import type { GridCssVars } from '../grid';
 import type { ActionSheetCssVars } from '../action-sheet';
+import type { ActivityIndicatorCssVars } from '../activity-indicator';
+import type { GridCssVars } from '../grid';
+import type { SliderCssVars } from '../slider';
 import type { SwitchCssVars } from '../switch';
 
-export type CssVars = GridCssVars & ActionSheetCssVars & SwitchCssVars;
+export type CssVars = ActionSheetCssVars &
+  ActivityIndicatorCssVars &
+  GridCssVars &
+  SliderCssVars &
+  SwitchCssVars;
