62b31956058ce769a07017847e7e5a9bab18e076
JeromeLin <jerome.lin@zhongan.com>
2022-02-16 10:12:54 +0800

    perf(affix): optimize styles and css vars define

--- packages/zarm/src/n-config-provider/cssVars.ts
+++ packages/zarm/src/n-config-provider/cssVars.ts
@@ -1,11 +1,13 @@
 import type { ActionSheetCssVars } from '../action-sheet';
 import type { ActivityIndicatorCssVars } from '../activity-indicator';
+import type { AffixCssVars } from '../affix';
 import type { GridCssVars } from '../grid';
 import type { SliderCssVars } from '../slider';
 import type { SwitchCssVars } from '../switch';
 
 export type CssVars = ActionSheetCssVars &
   ActivityIndicatorCssVars &
+  AffixCssVars &
   GridCssVars &
   SliderCssVars &
   SwitchCssVars;
