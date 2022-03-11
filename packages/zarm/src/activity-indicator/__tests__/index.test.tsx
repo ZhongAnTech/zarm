b74a53eadf03a5c8e44c263cf6372b33cf5e907f
JeromeLin <jerome.lin@zhongan.com>
2022-03-11 15:42:18 +0800

    perf: remove useless html attributes and optimize css vars

--- packages/zarm/src/activity-indicator/__tests__/index.test.tsx
+++ packages/zarm/src/activity-indicator/__tests__/index.test.tsx
@@ -13,22 +13,4 @@ describe('ActivityIndicator', () => {
     const wrapper = render(<ActivityIndicator type="spinner" />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
-
-  describe('Spinner', () => {
-    it('should render with HTML div element attributes', () => {
-      const wrapper = render(
-        <ActivityIndicator type="spinner" spellCheck style={{ width: '100px', height: '100px' }} />,
-      );
-      expect(toJson(wrapper)).toMatchSnapshot();
-    });
-  });
-
-  describe('Circular', () => {
-    it('should render with HTML div element attributes', () => {
-      const wrapper = render(
-        <ActivityIndicator spellCheck style={{ width: '100px', height: '100px' }} />,
-      );
-      expect(toJson(wrapper)).toMatchSnapshot();
-    });
-  });
 });
--- packages/zarm/src/activity-indicator/index.tsx
+++ packages/zarm/src/activity-indicator/index.tsx
@@ -1,7 +1,8 @@
-import React, { HTMLAttributes } from 'react';
+import * as React from 'react';
 import { createBEM } from '@zarm-design/bem';
 import { ConfigContext } from '../n-config-provider';
 import type { BaseActivityIndicatorProps } from './interface';
+import type { HTMLProps } from '../utils/utilityTypes';
 
 export interface ActivityIndicatorCssVars {
   '--size'?: React.CSSProperties['width' | 'height'];
@@ -14,109 +15,99 @@ export interface ActivityIndicatorCssVars {
   '--spinner-item-border-radius'?: React.CSSProperties['borderRadius'];
 }
 
+export type ActivityIndicatorProps = BaseActivityIndicatorProps &
+  HTMLProps<ActivityIndicatorCssVars> & {
+    onClick?: React.MouseEventHandler<HTMLDivElement>;
+  };
+
 const DIAMETER = 62;
 
-const Circular = React.forwardRef(
-  (
-    { className, size, percent, strokeWidth, loading, ...htmlAttributes }: ActivityIndicatorProps,
-    ref: any,
-  ) => {
-    const { prefixCls } = React.useContext(ConfigContext);
-    const bem = createBEM('activity-indicator', { prefixCls });
+const Circular = React.forwardRef<HTMLDivElement, ActivityIndicatorProps>((props, ref) => {
+  const { className, size, percent, strokeWidth, loading, ...restProps } = props;
+  const { prefixCls } = React.useContext(ConfigContext);
+  const bem = createBEM('activity-indicator', { prefixCls });
 
-    const cls = bem([
-      {
-        circular: loading,
-        [`${size}`]: !!size,
-      },
-      className,
-    ]);
+  const cls = bem([
+    {
+      circular: loading,
+      [`${size}`]: !!size,
+    },
+    className,
+  ]);
 
-    const half = DIAMETER / 2;
-    const r = half - (strokeWidth as number) / 2;
+  const half = DIAMETER / 2;
+  const r = half - (strokeWidth as number) / 2;
 
-    if (loading) {
-      return (
-        <div className={cls} {...htmlAttributes} ref={ref}>
-          <svg viewBox={`${DIAMETER / 2} ${DIAMETER / 2} ${DIAMETER} ${DIAMETER}`}>
-            <circle cx={DIAMETER} cy={DIAMETER} r={r} fill="none" style={{ strokeWidth }} />
-          </svg>
-        </div>
-      );
-    }
-
-    const round = 2 * Math.PI * r;
-    const lineStyle = {
-      strokeDasharray: `${(round * (percent as number)) / 100} ${round}`,
-      strokeWidth,
-    };
-
+  if (loading) {
     return (
-      <div className={cls} {...htmlAttributes} ref={ref}>
-        <svg viewBox={`0 0 ${DIAMETER} ${DIAMETER}`}>
-          <circle
-            className={bem('stroke')}
-            cx={half}
-            cy={half}
-            r={r}
-            fill="none"
-            style={{ strokeWidth }}
-          />
-          <circle className={bem('line')} cx={half} cy={half} r={r} fill="none" style={lineStyle} />
+      <div className={cls} {...restProps} ref={ref}>
+        <svg viewBox={`${DIAMETER / 2} ${DIAMETER / 2} ${DIAMETER} ${DIAMETER}`}>
+          <circle cx={DIAMETER} cy={DIAMETER} r={r} fill="none" style={{ strokeWidth }} />
         </svg>
       </div>
     );
-  },
-);
+  }
+
+  const round = 2 * Math.PI * r;
+  const lineStyle = {
+    strokeDasharray: `${(round * (percent as number)) / 100} ${round}`,
+    strokeWidth,
+  };
+
+  return (
+    <div className={cls} {...restProps} ref={ref}>
+      <svg viewBox={`0 0 ${DIAMETER} ${DIAMETER}`}>
+        <circle
+          className={bem('stroke')}
+          cx={half}
+          cy={half}
+          r={r}
+          fill="none"
+          style={{ strokeWidth }}
+        />
+        <circle className={bem('line')} cx={half} cy={half} r={r} fill="none" style={lineStyle} />
+      </svg>
+    </div>
+  );
+});
 
 Circular.displayName = 'Circular';
 
-type HTMLDivElementAttributeKeys = keyof HTMLAttributes<HTMLDivElement>;
-const Spinner = React.forwardRef(
-  (
+const Spinner = React.forwardRef<HTMLDivElement, ActivityIndicatorProps>((props, ref) => {
+  const { className, size, ...restProps } = props;
+  const { prefixCls } = React.useContext(ConfigContext);
+  const bem = createBEM('activity-indicator', { prefixCls });
+
+  const cls = bem([
     {
-      className,
-      size,
-      ...htmlAttributes
-    }: Pick<ActivityIndicatorProps, 'className' | 'size' | HTMLDivElementAttributeKeys>,
-    ref: any,
-  ) => {
-    const { prefixCls } = React.useContext(ConfigContext);
-    const bem = createBEM('activity-indicator', { prefixCls });
+      spinner: true,
+      [`${size}`]: !!size,
+    },
+    className,
+  ]);
 
-    const cls = bem([
-      {
-        spinner: true,
-        [`${size}`]: !!size,
-      },
-      className,
-    ]);
+  const spinner: React.ReactElement[] = [];
 
-    const spinner: React.ReactElement[] = [];
+  for (let i = 0; i < 9; i++) {
+    spinner.push(<div key={i} />);
+  }
 
-    for (let i = 0; i < 12; i++) {
-      spinner.push(<div key={i} />);
-    }
-
-    return (
-      <div className={cls} {...htmlAttributes} ref={ref}>
-        {spinner}
-      </div>
-    );
-  },
-);
+  return (
+    <div ref={ref} className={cls} {...restProps}>
+      {spinner}
+    </div>
+  );
+});
 
 Spinner.displayName = 'Spinner';
 
-export type ActivityIndicatorProps = BaseActivityIndicatorProps & HTMLAttributes<HTMLDivElement>;
-
-const ActivityIndicator = React.forwardRef<unknown, ActivityIndicatorProps>((props, ref) => {
+const ActivityIndicator = React.forwardRef<HTMLDivElement, ActivityIndicatorProps>((props, ref) => {
   if (props.type !== 'spinner') {
-    const { type, ...rest } = props;
-    return <Circular {...rest} ref={ref} />;
+    const { type, ...restProps } = props;
+    return <Circular ref={ref} {...restProps} />;
   }
-  const { strokeWidth, percent, loading, type, ...rest } = props;
-  return <Spinner {...rest} ref={ref} />;
+  const { strokeWidth, percent, loading, type, ...restProps } = props;
+  return <Spinner ref={ref} {...restProps} />;
 });
 
 ActivityIndicator.defaultProps = {
