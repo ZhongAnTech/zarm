61900564015b555ab4488c4421cb36c40dc8cdb1
Yang <yangpan@zhongan.com>
2022-05-16 15:12:06 +0800

    refactor: calendar

---
+++ packages/zarm/src/calendar/interface.ts
@@ -0,0 +1,29 @@
+import { ReactNode } from 'react';
+import type { Locale } from '../config-provider/PropsType';
+
+export interface BaseCalendarProps {
+  value?: Date | Date[];
+  defaultValue?: Date | Date[];
+  min?: Date;
+  max?: Date;
+  mode: 'single' | 'multiple' | 'range';
+  weekStartsOn?: 'Monday' | 'Sunday';
+  direction?: 'horizontal' | 'vertical';
+  dateRender?: (value?: Date) => ReactNode;
+  disabledDate?: (value?: Date) => boolean;
+  onChange?: (value?: Date[]) => void;
+  locale?: Locale['Calendar'];
+}
+
+export interface BaseCalendarMonthProps {
+  value: Date[];
+  min: Date;
+  max: Date;
+  dateMonth: Date;
+  mode: 'single' | 'multiple' | 'range';
+  weekStartsOn?: 'Monday' | 'Sunday';
+  dateRender?: (value?: Date) => ReactNode;
+  disabledDate?: (value?: Date) => boolean;
+  onDateClick?: (value?: Date) => void;
+  locale?: Locale['Calendar'];
+}
