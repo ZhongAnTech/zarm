1e0556fa909538745ff0e3bd5669288486286045
JeromeLin <jerome.lin@zhongan.com>
2022-06-23 19:32:47 +0800

    style: format code style

--- packages/zarm/src/calendar/locale/zh_CN.ts
+++ packages/zarm/src/calendar/locale/zh_CN.ts
@@ -2,5 +2,5 @@ export default {
   weeks: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
   months: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
   yearMonthFormat: 'YYYY年M月',
-  weekStartsOn: 'Monday'
+  weekStartsOn: 'Monday',
 };
