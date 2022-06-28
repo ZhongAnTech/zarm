9573179da80b96f5194993a67356cf2f9d6fa1ae
JeromeLin <jerome.lin@zhongan.com>
2022-06-28 10:35:16 +0800

    test(calendar): update tests

--- packages/zarm/src/calendar/__tests__/index.test.tsx
+++ packages/zarm/src/calendar/__tests__/index.test.tsx
@@ -3,14 +3,12 @@ import { mount } from 'enzyme';
 import Calendar from '../index';
 import { date7 } from '../../../tests/testData/date';
 
-const prefixCls = 'za-calendar';
-
 describe('Calendar', () => {
   it('should trigger onChange when single click', async () => {
     const onChangeFn = jest.fn();
     const wrapper = mount(<Calendar min={date7} mode="single" onChange={onChangeFn} />);
 
-    const day2 = wrapper.find(`.${prefixCls}__day`).at(15);
+    const day2 = wrapper.find('.za-calendar__day').at(15);
     expect(day2.childAt(0).text()).toBe('16');
     day2.simulate('click');
     expect(onChangeFn).toBeCalled();
@@ -18,10 +16,18 @@ describe('Calendar', () => {
 
   it('should trigger onChange when double click', () => {
     const onChangeFn = jest.fn();
-    const wrapper = mount(<Calendar mode="range" onChange={onChangeFn} />);
+    const wrapper = mount(
+      <Calendar
+        mode="range"
+        min={new Date('2022-01-01')}
+        max={new Date('2022-01-28')}
+        onChange={onChangeFn}
+      />,
+    );
 
-    const day3 = wrapper.find(`.${prefixCls}__day`).at(30);
-    const day4 = wrapper.find(`.${prefixCls}__day`).at(26);
+    const day3 = wrapper.find('.za-calendar__day').at(27);
+    const day4 = wrapper.find('.za-calendar__day').at(16);
+    console.log(day3);
     day3.simulate('click');
     day4.simulate('click');
     expect(onChangeFn).toBeCalled();
