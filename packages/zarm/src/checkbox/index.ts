0caa1af342cb00f3d6d07efcfd6323f10007bb60
JeromeLin <jerome.lin@zhongan.com>
2022-07-13 19:33:27 +0800

    style: code style fomat

--- packages/zarm/src/checkbox/index.ts
+++ packages/zarm/src/checkbox/index.ts
@@ -5,6 +5,4 @@ import attachPropertiesToComponent from '../utils/attachPropertiesToComponent';
 export type { CheckboxProps, CheckboxCssVars } from './Checkbox';
 export type { CheckboxGroupProps, CheckboxGroupCssVars } from './CheckboxGroup';
 
-export default attachPropertiesToComponent(Checkbox, {
-  Group,
-});
+export default attachPropertiesToComponent(Checkbox, { Group });
