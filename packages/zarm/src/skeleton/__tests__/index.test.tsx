549d4b61bfe63186d28a267743f17f9a0f83d93f
xiehailong <xiehailong@xiehailongdeMacBook-Pro.local>
2022-08-26 15:51:55 +0800

    chore: Skeleton支持暗黑主题& test demo

--- packages/zarm/src/skeleton/__tests__/index.test.tsx
+++ packages/zarm/src/skeleton/__tests__/index.test.tsx
@@ -1,11 +1,33 @@
 import * as React from 'react';
-import { render } from 'enzyme';
-import toJson from 'enzyme-to-json';
-import skeleton from '../index';
+import { cleanup, render, screen } from '@testing-library/react';
+import Skeleton from '../index';
 
-describe('skeleton', () => {
-  it('renders correctly', () => {
-    const wrapper = render(<skeleton />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+describe('Skeleton', () => {
+  afterEach(cleanup);
+
+  it('should render radius shape skeleton', () => {
+    render(<Skeleton data-testid="skeleton-root" />);
+    expect(screen.getByTestId('skeleton-root')).toBeTruthy();
+    expect(screen.getByTestId('skeleton-root').className).toEqual(
+      'za-skeleton za-skeleton--radius',
+    );
+  });
+
+  test.each`
+    className            | shape       | expected
+    ${'custom-skeleton'} | ${'circle'} | ${'za-skeleton za-skeleton--circle custom-skeleton'}
+    ${'custom-skeleton'} | ${'radius'} | ${'za-skeleton za-skeleton--radius custom-skeleton'}
+    ${'custom-skeleton'} | ${'rect'}   | ${'za-skeleton za-skeleton--rect custom-skeleton'}
+  `(
+    'should render with className - $className and shape - $shape',
+    ({ className, shape, expected }) => {
+      render(<Skeleton data-testid="skeleton-root" className={className} shape={shape} />);
+      expect(screen.getByTestId('skeleton-root').className).toEqual(expected);
+    },
+  );
+
+  test('should render with default props', () => {
+    const { container } = render(<Skeleton />);
+    expect(container).toMatchSnapshot();
   });
 });
