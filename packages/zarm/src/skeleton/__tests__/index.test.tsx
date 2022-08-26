87d3b5384a6c99d47d368804083874d0b49716a7
xiehailong <xiehailong@xiehailongdeMacBook-Pro.local>
2022-08-26 18:04:08 +0800

    test: add test demo

--- packages/zarm/src/skeleton/__tests__/index.test.tsx
+++ packages/zarm/src/skeleton/__tests__/index.test.tsx
@@ -5,6 +5,11 @@ import Skeleton from '../index';
 describe('Skeleton', () => {
   afterEach(cleanup);
 
+  test('should render with default props', () => {
+    const { container } = render(<Skeleton />);
+    expect(container).toMatchSnapshot();
+  });
+
   it('should render radius shape skeleton', () => {
     render(<Skeleton data-testid="skeleton-root" />);
     expect(screen.getByTestId('skeleton-root')).toBeTruthy();
@@ -26,8 +31,61 @@ describe('Skeleton', () => {
     },
   );
 
-  test('should render with default props', () => {
-    const { container } = render(<Skeleton />);
-    expect(container).toMatchSnapshot();
+  it('should render animated skeleton', () => {
+    render(<Skeleton animated data-testid="skeleton-root" />);
+    expect(screen.getByTestId('skeleton-root')).toBeTruthy();
+    expect(screen.getByTestId('skeleton-root').className).toEqual(
+      'za-skeleton za-skeleton--radius za-skeleton--animated',
+    );
+  });
+
+  it('should render Skeleton.Title', () => {
+    render(<Skeleton.Title data-testid="skeleton-root" />);
+    expect(screen.getByTestId('skeleton-root')).toBeTruthy();
+    expect(screen.getByTestId('skeleton-root').className).toEqual(
+      'za-skeleton za-skeleton--radius za-skeleton__title',
+    );
+  });
+
+  it('should render rect Skeleton.Title', () => {
+    render(<Skeleton.Title shape="rect" data-testid="skeleton-root" />);
+    expect(screen.getByTestId('skeleton-root')).toBeTruthy();
+    expect(screen.getByTestId('skeleton-root').className).toEqual(
+      'za-skeleton za-skeleton--rect za-skeleton__title',
+    );
+  });
+
+  it('should render animated Skeleton.Title', () => {
+    render(<Skeleton.Title animated data-testid="skeleton-root" />);
+    expect(screen.getByTestId('skeleton-root')).toBeTruthy();
+    expect(screen.getByTestId('skeleton-root').className).toEqual(
+      'za-skeleton za-skeleton--radius za-skeleton--animated za-skeleton__title',
+    );
+  });
+
+  it('should render Skeleton.Paragraph', () => {
+    render(<Skeleton.Paragraph lineCount={5} data-testid="skeleton-root" />);
+    expect(screen.getByTestId('skeleton-root')).toBeTruthy();
+    expect(screen.getByTestId('skeleton-root').className).toEqual('za-skeleton__paragraph');
+    expect(screen.getByTestId('skeleton-root').firstElementChild!.className).toEqual(
+      'za-skeleton za-skeleton--radius za-skeleton__line',
+    );
+    expect(screen.getByTestId('skeleton-root').childElementCount).toBe(5);
+  });
+
+  it('should render animated Skeleton.Paragraph', () => {
+    render(<Skeleton.Paragraph animated data-testid="skeleton-root" />);
+    expect(screen.getByTestId('skeleton-root')).toBeTruthy();
+    expect(screen.getByTestId('skeleton-root').firstElementChild!.className).toEqual(
+      'za-skeleton za-skeleton--radius za-skeleton--animated za-skeleton__line',
+    );
+  });
+
+  it('should render rect Skeleton.Paragraph', () => {
+    render(<Skeleton.Paragraph shape="rect" data-testid="skeleton-root" />);
+    expect(screen.getByTestId('skeleton-root')).toBeTruthy();
+    expect(screen.getByTestId('skeleton-root').firstElementChild!.className).toEqual(
+      'za-skeleton za-skeleton--rect za-skeleton__line',
+    );
   });
 });
