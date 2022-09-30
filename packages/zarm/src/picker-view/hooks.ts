2149b154e9506380beacfd17e34cd2f030a51174
dominicleo <dominicleo@mail.com>
2022-09-30 18:19:34 +0800

    chore: parseProps

---
+++ packages/zarm/src/picker-view/hooks.ts
@@ -0,0 +1,7 @@
+import type { PickerDataSourceItem } from './interface';
+
+export const isCascader = () => {};
+
+export const useDataSource = (dataSource?: PickerDataSourceItem[]) => {
+  return dataSource;
+};
