841c30deac7cc874e43447c6e7347722f0308c77
Yang <yangpan@zhongan.com>
2022-11-04 17:13:43 +0800

    refactor: button test 去掉enzyme

--- packages/zarm/src/button/__tests__/index.test.jsx
+++ packages/zarm/src/button/__tests__/index.test.jsx
@@ -1,87 +1,80 @@
 import React from 'react';
-import { render, shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Button from '../index';
 
 describe('Button', () => {
   it('renders correctly', () => {
     const wrapper = render(<Button>foo</Button>);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.asFragment()).toMatchSnapshot();
   });
 
   it('theme', () => {
     const wrapper = render(<Button theme="primary">foo</Button>);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.asFragment()).toMatchSnapshot();
   });
 
   it('block', () => {
     const wrapper = render(<Button block>foo</Button>);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.asFragment()).toMatchSnapshot();
   });
 
   it('ghost', () => {
     const wrapper = render(<Button ghost>foo</Button>);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.asFragment()).toMatchSnapshot();
   });
 
   it('size', () => {
     const wrapper = render(<Button size="lg">foo</Button>);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.asFragment()).toMatchSnapshot();
   });
 
   it('shape is radius', () => {
     const wrapper = render(<Button shape="radius">foo</Button>);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.asFragment()).toMatchSnapshot();
   });
 
   it('icon', () => {
     const wrapper = render(
       <Button icon={<img alt="" src="https://zarm.design/images/logo.ce68565d.svg" />}>foo</Button>,
     );
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.asFragment()).toMatchSnapshot();
   });
 
   it('loading', () => {
     const wrapper = render(<Button loading>foo</Button>);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.asFragment()).toMatchSnapshot();
   });
 
   it('disabled', () => {
     const wrapper = render(<Button disabled>foo</Button>);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.asFragment()).toMatchSnapshot();
   });
 
   it('htmlType', () => {
     const wrapper = render(<Button htmlType="submit">foo</Button>);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.asFragment()).toMatchSnapshot();
   });
 
   it('onClick', () => {
     const onClick = jest.fn();
-    const wrapper = shallow(<Button onClick={onClick}>foo</Button>);
-    wrapper.simulate('click');
+    render(<Button onClick={onClick}>button click</Button>);
+    const btn = screen.getByText('button click');
+    fireEvent.click(btn);
     expect(onClick).toBeCalled();
   });
 
   it('onClick when disabled', () => {
     const onClick = jest.fn();
-    const wrapper = shallow(
-      <Button disabled onClick={onClick}>
-        foo
-      </Button>,
+    render(
+      <div data-testid="za-btn">
+        <Button disabled onClick={onClick}>
+          button disable
+        </Button>
+      </div>,
     );
-    wrapper.simulate('click');
+    const btn = screen.getByText('button disable');
+    fireEvent.click(btn);
     expect(onClick).not.toBeCalled();
-  });
-
-  it('onTouchStart in NativeButton', () => {
-    const wrapper = shallow(<Button>foo</Button>);
-    wrapper.simulate('touchstart');
-  });
-
-  it('onTouchStart in AnchorButton', () => {
-    const wrapper = shallow(<Button href="https://zarm.design">foo</Button>);
-    wrapper.simulate('touchstart');
   });
 
   it('href and target', () => {
@@ -90,6 +83,6 @@ describe('Button', () => {
         foo
       </Button>,
     );
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.asFragment()).toMatchSnapshot();
   });
 });
