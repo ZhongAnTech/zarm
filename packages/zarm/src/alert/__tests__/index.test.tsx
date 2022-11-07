f17b1f65e09edf20fb54393bab70ab06b653894f
question <yangpan103@gmail.com>
2022-11-07 17:24:45 +0800

    refactor: 更新测试用例 (#968)
    * refactor: activity-indicator 测试去掉enzyme
    
    * refactor: affix 测试去掉enzyme
    
    * refactor: button test 去掉enzyme
    
    * refactor: 替换alert 测试用例
    
    Co-authored-by: Yang <yangpan@zhongan.com>

--- packages/zarm/src/alert/__tests__/index.test.tsx
+++ packages/zarm/src/alert/__tests__/index.test.tsx
@@ -1,12 +1,10 @@
-import { shallow } from 'enzyme';
 import React from 'react';
-import Modal from '../../modal';
+import { render } from '@testing-library/react';
 import Alert from '../Alert';
 
 describe('Alert', () => {
   it('should render correctly', () => {
-    const wrapper = shallow(<Alert animationType="door" />);
-    expect(wrapper.find(Modal)).toHaveLength(1);
-    expect(wrapper.prop('className')).toEqual('za-alert');
+    render(<Alert animationType="door" visible />);
+    expect(document.body.querySelectorAll('.za-alert')).toHaveLength(1);
   });
 });
