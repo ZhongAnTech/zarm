dd66edede3b38d112ba3f007bbd6dde2deecb371
Yang <yangpan@zhongan.com>
2022-11-07 20:21:10 +0800

    refactor: update calendar test

--- packages/zarm/src/calendar/__tests__/index.test.tsx
+++ packages/zarm/src/calendar/__tests__/index.test.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { fireEvent, getByText, render } from '@testing-library/react';
 import Calendar from '../index';
+
 import { date7 } from '../../../tests/testData/date';
 
 describe('Calendar', () => {
   it('should trigger onChange when single click', async () => {
     const onChangeFn = jest.fn();
-    const wrapper = mount(<Calendar min={date7} mode="single" onChange={onChangeFn} />);
+    const { container } = render(<Calendar min={date7} mode="single" onChange={onChangeFn} />);
 
-    const day2 = wrapper.find('.za-calendar__day').at(15);
-    expect(day2.childAt(0).text()).toBe('16');
-    day2.simulate('click');
+    const day2 = [].slice.call(container.getElementsByClassName('za-calendar__day'))[15];
+    expect(getByText(day2, '16')).toBeTruthy();
+    fireEvent.click(day2);
     expect(onChangeFn).toBeCalled();
   });
 
   it('should trigger onChange when double click', () => {
     const onChangeFn = jest.fn();
-    const wrapper = mount(
+    const { container } = render(
       <Calendar
         mode="range"
         min={new Date('2022-01-01')}
@@ -25,10 +26,9 @@ describe('Calendar', () => {
       />,
     );
 
-    const day3 = wrapper.find('.za-calendar__day').at(27);
-    const day4 = wrapper.find('.za-calendar__day').at(16);
-    day3.simulate('click');
-    day4.simulate('click');
+    const days = [].slice.call(container.getElementsByClassName('za-calendar__day'));
+    fireEvent.click(days[27]);
+    fireEvent.click(days[16]);
     expect(onChangeFn).toBeCalled();
   });
 });
