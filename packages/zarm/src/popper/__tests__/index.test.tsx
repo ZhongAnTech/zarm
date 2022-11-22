f7d3dfeb0c9a1e0182c67304a6e50b75646ebe93
Yang <yangpan@zhongan.com>
2022-11-22 15:27:10 +0800

    refactor: popper test使用ts

--- packages/zarm/src/popper/__tests__/index.test.tsx
+++ packages/zarm/src/popper/__tests__/index.test.tsx
@@ -6,6 +6,7 @@ if (global.document) {
   document.createRange = () => ({
     setStart: () => {},
     setEnd: () => {},
+    // @ts-ignore
     commonAncestorContainer: {
       nodeName: 'BODY',
       ownerDocument: document,
