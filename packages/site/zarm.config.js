39d6a2aa7328fb7781654ed4de7036c56624a213
dominicleo <dominicleo@mail.com>
2022-12-16 21:01:17 +0800

    docs: add diff example

--- packages/site/zarm.config.js
+++ packages/site/zarm.config.js
@@ -29,7 +29,7 @@ module.exports = {
     options.plugins.push([
       'prismjs',
       {
-        languages: ['javascript', 'typescript', 'jsx', 'tsx', 'css', 'scss', 'markup', 'bash'],
+        languages: ['javascript', 'typescript', 'jsx', 'tsx', 'css', 'scss', 'markup', 'bash', 'diff'],
         theme: 'default',
         css: true,
       },
