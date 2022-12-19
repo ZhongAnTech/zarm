87229d6fb54d380ec40e1cc97499575c9f9ab07f
Yang <yangpan@zhongan.com>
2022-12-19 17:48:59 +0800

    refactor: @zarm-design/bem/sass 替换~@zarm-design/bem/sass

--- packages/site/zarm.config.js
+++ packages/site/zarm.config.js
@@ -26,7 +26,7 @@ module.exports = {
   },
   setBabelOptions: (options) => {
     options.plugins.push(['import', { libraryName: 'zarm-web', style: 'css' }, 'zarm-web']);
-    // options.plugins.push(['import', { libraryName: 'antd', style: 'css' }, 'antd']);
+    options.plugins.push(['import', { libraryName: 'antd', style: 'css' }, 'antd']);
     options.plugins.push([
       'prismjs',
       {
