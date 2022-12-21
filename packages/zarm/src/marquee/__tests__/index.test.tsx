9a7e8c8cea9e3a1ec85325e999ae2e2ec8030b39
Yang <yangpan@zhongan.com>
2022-12-21 22:15:57 +0800

    test: improve marquee test coverage

--- packages/zarm/src/marquee/__tests__/index.test.tsx
+++ packages/zarm/src/marquee/__tests__/index.test.tsx
@@ -23,16 +23,51 @@ describe('Marquee', () => {
     expect(container).toMatchSnapshot();
   });
 
-  // it('component props', () => {
-  //   const upMarquee = mount(createMarquee({ direction: 'up' }));
-  //   const leftMarquee = mount(createMarquee({ direction: 'left' }));
-  //   const rightMarquee = mount(createMarquee({ direction: 'right' }));
-  //   const downMarquee = mount(createMarquee({ direction: 'down' }));
-  //   const marquee = mount(createMarquee({ direction: '' }));
-  //   expect(leftMarquee.props().direction).toBe('left');
-  //   expect(upMarquee.props().direction).toBe('up');
-  //   expect(rightMarquee.props().direction).toBe('right');
-  //   expect(downMarquee.props().direction).toBe('down');
-  //   expect(marquee.props().direction).toBe('');
-  // });
+  it('renders correctly top', () => {
+    const { container } = render(
+      <div>
+        {createMarquee({
+          height: 150,
+          direction: 'left',
+          width: '100%',
+          loop: false,
+          delay: 5000,
+          prefix: 'zarm-marquee',
+        })}
+      </div>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders correctly down', () => {
+    const { container } = render(
+      <div>
+        {createMarquee({
+          height: 150,
+          direction: 'down',
+          width: 50,
+          loop: false,
+          delay: 5000,
+          prefix: 'zarm-marquee',
+        })}
+      </div>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders correctly up', () => {
+    const { container } = render(
+      <div>
+        {createMarquee({
+          height: 150,
+          direction: 'up',
+          width: 50,
+          loop: false,
+          delay: 5000,
+          prefix: 'zarm-marquee',
+        })}
+      </div>,
+    );
+    expect(container).toMatchSnapshot();
+  });
 });
