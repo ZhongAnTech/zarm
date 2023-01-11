83cc6fa4cb6b1ba0c8e573cef8d2a9ba0045e24f
Yang <yangpan@zhongan.com>
2023-01-11 10:00:51 +0800

    refactor: remove unUsed code

--- packages/zarm/src/click-outside/PropsType.tsx
+++
@@ -1,8 +0,0 @@
-import type { HTMLAttributes } from 'react';
-
-export default interface ClickOutsideProps extends HTMLAttributes<HTMLDivElement> {
-  onClickOutside?: (event: Event) => void;
-  disabled?: boolean;
-  className?: string;
-  ignoredNode?: HTMLElement;
-}
