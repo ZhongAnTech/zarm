4d46b35f8b6b14964fd3b806e99fc090a61d3129
question <yangpan103@gmail.com>
2023-02-23 19:33:14 +0800

    test: improve test coverage (#1046)
    Co-authored-by: Yang <yangpan@zhongan.com>

--- packages/zarm/src/date-picker/__tests__/index.test.tsx
+++ packages/zarm/src/date-picker/__tests__/index.test.tsx
@@ -1,6 +1,7 @@
 import { fireEvent, render } from '@testing-library/react';
 import React from 'react';
 import DatePicker from '../index';
+import Button from '../../button';
 
 describe('DatePicker', () => {
   it('snapshot', () => {
@@ -22,4 +23,34 @@ describe('DatePicker', () => {
     fireEvent.click(document.body.querySelectorAll('.za-picker__cancel')[0]);
     expect(onCancelFn).toBeCalled();
   });
+
+  it('static method prompt', async () => {
+    const confirm = jest.fn();
+    const cancel = jest.fn();
+    const { getByText } = render(
+      <Button
+        size="xs"
+        onClick={async () => {
+          await DatePicker.prompt(
+            {
+              defaultValue: new Date('2023/2/23'),
+              onConfirm: confirm,
+              onCancel: cancel,
+              confirmText: 'date picker confirm',
+              cancelText: 'date picker cancel'
+            }
+          );
+        }}
+      >
+        date picker
+      </Button>
+    )
+    const btn = getByText('date picker');
+    fireEvent.click(btn);
+    fireEvent.click(getByText('date picker confirm'));
+    expect(confirm).toHaveBeenCalledWith(new Date('2023/2/23'), [{"label": "2023年", "value": 2023}, {"label": "2月", "value": 2}, {"label": "23日", "value": 23}]);
+    // fireEvent.click(btn);
+    fireEvent.click(getByText('date picker cancel'));
+    expect(cancel).toBeCalled();
+  })
 });
