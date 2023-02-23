69b169c49385e5672d2204f6c0d12ce985357a17
Yang <yangpan@zhongan.com>
2023-02-23 18:44:30 +0800

    test: improve test coverage

--- packages/zarm/src/date-picker-view/__tests__/index.test.tsx
+++ packages/zarm/src/date-picker-view/__tests__/index.test.tsx
@@ -11,4 +11,14 @@ describe('DatePickerView', () => {
     const wrapper = render(<DatePickerView defaultValue={new Date('2017/12/3 14:00')} />);
     expect(wrapper.asFragment()).toMatchSnapshot();
   });
+
+  it('meridiem hour snapshot', () => {
+    const wrapper = render(<DatePickerView defaultValue={new Date('2017/12/3 14:00')} columnType={['meridiem', 'hour', 'minute']} />);
+    expect(wrapper.asFragment()).toMatchSnapshot();
+  });
+
+  it('week snapshot', () => {
+    const wrapper = render(<DatePickerView defaultValue={new Date('2017/12/3 14:00')} columnType={['year', 'week', 'week-day']} />);
+    expect(wrapper.asFragment()).toMatchSnapshot();
+  });
 });
