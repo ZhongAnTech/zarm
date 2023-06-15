a1e629cec03cbe89528f4e4bf8ba4ed9f2d908b6
Yang <yangpan@zhongan.com>
2023-06-15 10:12:54 +0800

    feat: 小程序doc

--- packages/mini-demo/sync.js
+++ packages/mini-demo/sync.js
@@ -4,7 +4,7 @@ const fsExtra = require('fs-extra');
 chokidar.watch('../../packages/zarm/src/**/demo/*mini.tsx').on('change', async (path) => {
   const dir = path.match(/[A-Z a-z]+/g);
   const componentName = dir[3];
-  let content = await fsExtra.readFile(path, 'utf-8');
+  const content = await fsExtra.readFile(path, 'utf-8');
   const newContent = content.replace(
     /(\s*\/\*[\s\S][style placeholder]*?\*\/)/g,
     "\nimport './index.scss';",
