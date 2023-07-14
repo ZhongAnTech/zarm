37eb1c0dbf64e055e71c5b746acd3e41802c165d
Yang <yangpan@zhongan.com>
2023-07-14 10:05:29 +0800

    chore: add useTabs

---
+++ packages/zarm/src/tabs/useTabs.ts
@@ -0,0 +1,23 @@
+import { useControllableValue } from 'ahooks';
+import React, { Children } from 'react';
+import { TabPanelProps } from './TabPanel';
+
+const getChildChecked = (children: TabPanelProps['children']) => {
+  let selectIndex;
+  React.Children.forEach(children, (item, index) => {
+    if (React.isValidElement(item) && item.props && item.props.selected) {
+      selectIndex = index;
+    }
+  });
+  return selectIndex;
+};
+const useTabs = (props) => {
+  const [currentValue, setCurrentValue] = useControllableValue(props);
+
+  let value = (currentValue ?? getChildChecked(props.children)) || 0;
+
+  value = Math.min(Children.count(props.children) - 1, Math.max(0, value));
+  return [value, setCurrentValue];
+};
+
+export default useTabs;
