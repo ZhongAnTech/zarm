30971d804328578b891847ef15d2ed3bf1236ec3
Yang <yangpan@zhongan.com>
2023-08-17 14:20:51 +0800

    chore: input mini

---
+++ packages/zarm/src/input/index.mini.ts
@@ -0,0 +1,11 @@
+import Input from './Input.mini';
+
+export type {
+  InputCssVars,
+  InputProps,
+  InputRef,
+  InputTextareaProps,
+  InputTextProps,
+} from './Input';
+
+export default Input;
