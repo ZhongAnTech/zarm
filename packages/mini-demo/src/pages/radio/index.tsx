d181aa685891df63121858f84ac283336af0d20f
王梦良 <wangmengliang@wangmengliangdeMacBook-Pro.local>
2023-08-29 10:52:55 +0800

    chore: radio mini demo

--- packages/mini-demo/src/pages/radio/index.tsx
+++ packages/mini-demo/src/pages/radio/index.tsx
@@ -1,91 +1,17 @@
-import { View } from '@tarojs/components';
 import * as React from 'react';
-import { List, Radio } from 'zarm/mini';
+import Basic from './component/basic';
+import Button from './component/button';
+import Group from './component/group';
+import List from './component/list';
 import './index.scss';
 
-const Demo = () => {
-  const [value, setValue] = React.useState([]);
-
-  const onChange = (value) => {
-    console.log('onChange', value);
-    setValue(value);
-  };
-
+export default () => {
   return (
-    <View style={{ padding: 20 }}>
-      <View>基本用法</View>
-      <List>
-        <List.Item>
-          <Radio>普通</Radio>
-        </List.Item>
-        <List.Item>
-          <Radio defaultChecked>默认选中</Radio>
-        </List.Item>
-        <List.Item>
-          <Radio disabled>禁用</Radio>
-        </List.Item>
-        <List.Item>
-          <Radio defaultChecked disabled>
-            选中且禁用
-          </Radio>
-        </List.Item>
-      </List>
-      <View>组合使用</View>
-      <List>
-        <List.Item>
-          <Radio.Group value={value} onChange={onChange}>
-            <Radio value="0">选项一</Radio>
-            <Radio value="1">选项二</Radio>
-            <Radio value="2">选项三</Radio>
-          </Radio.Group>
-        </List.Item>
-      </List>
-      <View>列表样式</View>
-      <Radio.Group type="list">
-        <Radio value="0">选项一</Radio>
-        <Radio value="1">选项二</Radio>
-        <Radio value="2" disabled>
-          选项三（禁止选择）
-        </Radio>
-      </Radio.Group>
-      <View>通栏样式</View>
-      <List.Item>
-        <Radio.Group block>
-          <Radio value="0">选项一</Radio>
-          <Radio value="1">选项二</Radio>
-        </Radio.Group>
-      </List.Item>
-      <View>按钮样式</View>
-      <List.Item title="普通">
-        <Radio.Group type="button">
-          <Radio value="0">选项一</Radio>
-          <Radio value="1">选项二</Radio>
-          <Radio value="2">选项三</Radio>
-        </Radio.Group>
-      </List.Item>
-      <List.Item title="禁用">
-        <Radio.Group type="button" disabled>
-          <Radio value="0">选项一</Radio>
-          <Radio value="1">选项二</Radio>
-          <Radio value="2">选项三</Radio>
-        </Radio.Group>
-      </List.Item>
-      <List.Item title="通栏">
-        <Radio.Group type="button" block>
-          <Radio value="0">选项一</Radio>
-          <Radio value="1">选项二</Radio>
-          <Radio value="2">选项三</Radio>
-        </Radio.Group>
-      </List.Item>
-      <List.Item title="紧凑">
-        <Radio.Group type="button" compact>
-          <Radio value="0">选项一</Radio>
-          <Radio value="1">选项二</Radio>
-          <Radio value="2">选项三</Radio>
-        </Radio.Group>
-      </List.Item>
-    </View>
+    <>
+      <Basic />
+      <Group />
+      <List />
+      <Button />
+    </>
   );
 };
-
-export default Demo;
--- packages/zarm/src/radio/style/index.mini.ts
+++ packages/zarm/src/radio/style/index.mini.ts
@@ -1,4 +1,5 @@
+import '../../button/style/index.mini';
 import '../../icon/style';
-import '../../list/style';
+import '../../list/style/index.mini';
 import '../../style';
 import './index.scss';
