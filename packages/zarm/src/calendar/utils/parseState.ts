0978d46c262905e43736d434066b97b1ec931b98
question <yangpan103@gmail.com>
2023-11-15 16:20:16 +0800

    perf(calendar): performance optimization (#1155)
    Co-authored-by: Yang <yangpan@zhongan.com>

--- packages/zarm/src/calendar/utils/parseState.ts
+++ packages/zarm/src/calendar/utils/parseState.ts
@@ -1,14 +1,11 @@
 import dayjs from 'dayjs';
 
 const parseState = (props: {
-  min?: Date;
-  max?: Date;
   value?: Date | Date[];
   defaultValue?: Date | Date[];
   mode: string;
-  direction?: string;
 }) => {
-  const { defaultValue, mode, direction } = props;
+  const { defaultValue, mode } = props;
   let { value } = props;
 
   let tmpValue: Date[];
@@ -22,26 +19,8 @@ const parseState = (props: {
   if (mode === 'range') {
     tmpValue = [tmpValue[0], tmpValue[tmpValue.length - 1]];
   }
-
-  const min = props.min ? dayjs(props.min).toDate() : dayjs().toDate();
-  // const startMonth = dayjs(min).toDate();
-  const max = props.max ? dayjs(props.max).toDate() : dayjs(min).add(1, 'year').toDate();
-
-  // min、max 排序
-  const duration = [min, max].sort((item1: Date, item2: Date) => +item1 - +item2);
-  const steps = mode === 'range' ? 2 : 1;
   return {
     value: tmpValue,
-    min: duration[0],
-    max: duration[1],
-    // 是否是入参更新(主要是月份跨度更新，需要重新定位)
-    refresh: false,
-    // 注掉该逻辑，强制根据 multiple 控制节点个数，后面改进
-    // steps:Math.max(tmp.value.length, tmp.defaultValue.length);
-    steps,
-    // 初始化点击步数
-    mode,
-    direction,
   };
 };
 
